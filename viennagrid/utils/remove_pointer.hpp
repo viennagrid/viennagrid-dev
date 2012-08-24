bbe37671a5147cca55b68b075a24f95fb28ffda4
Florian Rudolf <florian@frudo.(none)>
2012-08-24 12:20:41 +0200

    boost::type_trais like remove_pointer

---
+++ viennagrid/utils/remove_pointer.hpp
@@ -0,0 +1,23 @@
+#ifndef VIENNAGRID_REMOVE_POINTER_HPP
+#define VIENNAGRID_REMOVE_POINTER_HPP
+
+namespace viennagrid
+{
+    namespace utils
+    {
+        
+        template<typename pointer_type>
+        struct remove_pointer;
+        
+        template<typename pointer_type>
+        struct remove_pointer<pointer_type*>
+        {
+            typedef pointer_type type;
+        };
+        
+        
+    }
+    
+}
+
+#endif
