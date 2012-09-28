810cc79a667f4443d223c9650d90a07bd1fbb586
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2012-09-28 16:02:23 +0200

    added view container tag added tag for reference type of a view remove view-container dependency

--- viennagrid/storage/view.hpp
+++ viennagrid/storage/view.hpp
@@ -1,5 +1,5 @@
-#ifndef VIENNAGRID_VIEW_HPP
-#define VIENNAGRID_VIEW_HPP
+#ifndef VIENNAGRID_STORAGE_VIEW_HPP
+#define VIENNAGRID_STORAGE_VIEW_HPP
 
 #include "container.hpp"
 
@@ -10,27 +10,39 @@ namespace viennagrid
     namespace storage
     {
         
-        template<typename base_container_type, typename view_container_tag = std_deque_tag>
-        class view_t;
+        struct pointer_reference_tag;
+        struct iterator_reference_tag;
+        struct id_tag;
         
-        namespace container
-        {
-            
-            template<typename base_container_type, typename view_container_tag>
-            typename viennagrid::storage::view_t<base_container_type, view_container_tag>::iterator insert(
-                    viennagrid::storage::view_t<base_container_type, view_container_tag> & container,
-                    const typename viennagrid::storage::view_t<base_container_type, view_container_tag>::value_type & element )
-            {
-                return container.insert( element );
-            }
-            
-        }
         
+        
+        
+        
+        template<typename base_container_type, typename view_container_tag, typename view_reference_tag>
+        struct view_container
+        {};
         
         template<typename base_container_type, typename view_container_tag>
+        struct view_container<base_container_type, view_container_tag, pointer_reference_tag>
+        {
+            typedef typename viennagrid::storage::result_of::container_from_tag<typename base_container_type::pointer, view_container_tag>::type type;
+        };
+        
+        template<typename base_container_type, typename view_container_tag>
+        struct view_container<base_container_type, view_container_tag, iterator_reference_tag>
+        {
+            typedef typename viennagrid::storage::result_of::container_from_tag<typename base_container_type::iterator, view_container_tag>::type type;
+        };
+        
+        
+        
+        
+        template<typename base_container_type, typename view_container_tag = std_deque_tag, typename view_reference_tag = pointer_reference_tag>
         class view_t
         {
-            typedef typename viennagrid::storage::result_of::container_from_tag<typename base_container_type::pointer, view_container_tag>::type view_container_type;
+            //typedef typename viennagrid::storage::result_of::container_from_tag<typename base_container_type::pointer, view_container_tag>::type view_container_type;
+            
+            typedef typename view_container<base_container_type, view_container_tag, view_reference_tag>::type view_container_type;
             
         public:
             
@@ -86,25 +98,15 @@ namespace viennagrid
             };
             
             
-            
-            view_t(base_container_type & _bc) : base_container(&_bc)
-            {
-                for (typename base_container_type::iterator it = _bc.begin(); it != _bc.end(); ++it)
-                    container.push_back( &(*it) );
-            }
-
-            template<class functor>
-            view_t(base_container_type & _bc, functor f) : base_container(&_bc)
-            {
-                for (typename base_container_type::iterator it = _bc.begin(); it != _bc.end(); ++it)
-                    if (f(*it)) container.push_back( &(*it) );
-            }
-            
-            
+            view_t() {}
             
             
             iterator begin() { return iterator(container.begin()); }
             iterator end() { return iterator(container.end()); }
+
+            const_iterator begin() const { return const_iterator(container.begin()); }
+            const_iterator end() const { return const_iterator(container.end()); }
+
             
             reference front() { return *(container.front()); }
             const_reference front() const { return *(container.front()); }
@@ -112,26 +114,51 @@ namespace viennagrid
             reference back() { return *(container.back()); }
             const_reference back() const { return *(container.back()); }
             
-            iterator insert(const value_type & element)
+            
+            
+            size_type size() const { return container.size(); }
+            
+            
+            iterator insert(value_type & element)
             {
                 return viennagrid::storage::container::insert(
                     container,
-                    &(*(viennagrid::storage::container::insert( *base_container, element )))
+                    &element
                 );
-            }
-            
-            iterator erase(const iterator & it)
-            {
-                return container.erase( it );                
             }
             
             
         private:
             
-            base_container_type * base_container;
             view_container_type container;
         };
+        
+        namespace container
+        {
+            template<typename base_container_type, typename view_container_tag, typename view_reference_tag, typename value_type>
+            typename viennagrid::storage::view_t<base_container_type, view_container_tag, view_reference_tag>::iterator insert(
+                    viennagrid::storage::view_t<base_container_type, view_container_tag, view_reference_tag> & container,
+                    value_type & element)
+            {
+                return container.insert( element );
+            }
+            
+            
+            template<typename from_iterator_type, typename to_container_type>
+            void reference( from_iterator_type it_start, from_iterator_type it_end, to_container_type & to )
+            {
+                for ( ;it_start != it_end; ++it_start)
+                    viennagrid::storage::container::insert(to, *it_start);
+            }
+        }
 
+        
+        template<typename _view_container_tag, typename _view_reference_tag = pointer_reference_tag>
+        struct view_tag
+        {
+            typedef _view_container_tag view_container_tag;
+            typedef _view_reference_tag view_reference_tag;
+        };
 
         namespace result_of
         {
@@ -141,6 +168,11 @@ namespace viennagrid
                 typedef view_t<base_container_type, view_container_tag> type;
             };
             
+            template<typename element_type, typename view_container_tag>
+            struct container_from_tag<element_type, view_tag<view_container_tag> >
+            {
+                typedef typename view<element_type, view_container_tag>::type type;
+            };
         }
 
 
