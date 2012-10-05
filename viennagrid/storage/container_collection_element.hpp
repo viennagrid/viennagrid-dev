b0668bcd4ae1c9d195046431229cc0bc96104746
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2012-10-05 11:51:27 +0200

    insert_callback now takes a bool parameter if the element was inserted or if it was present generic set_id function

--- viennagrid/storage/container_collection_element.hpp
+++ viennagrid/storage/container_collection_element.hpp
@@ -84,77 +84,106 @@ namespace viennagrid
         namespace container_collection_element
         {
             template<typename container_collection_element_type, typename inserter_type>
-            void insert_callback( container_collection_element_type & element, inserter_type & inserter)
+            void insert_callback( container_collection_element_type & element, bool insert_result, inserter_type & inserter)
             {
-                element.insert_callback( inserter );
+                element.insert_callback( inserter, insert_result );
+            }
+            
+            template<typename container_collection_element_type, typename inserter_type>
+            void insert_callback( const container_collection_element_type & element, bool insert_result, inserter_type & inserter)
+            {
+                element.insert_callback( inserter, insert_result );
             }
             
             template<typename inserter_type>
-            void insert_callback( bool & element, inserter_type & inserter) {}
+            void insert_callback( bool & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( char & element, inserter_type & inserter) {}
+            void insert_callback( char & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( unsigned char & element, inserter_type & inserter) {}
+            void insert_callback( unsigned char & element, bool insert_result, inserter_type & inserter) {}
             
             template<typename inserter_type>
-            void insert_callback( short & element, inserter_type & inserter) {}
+            void insert_callback( short & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( unsigned short & element, inserter_type & inserter) {}
+            void insert_callback( unsigned short & element, bool insert_result, inserter_type & inserter) {}
             
             template<typename inserter_type>
-            void insert_callback( int & element, inserter_type & inserter) {}
+            void insert_callback( int & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( unsigned int & element, inserter_type & inserter) {}
+            void insert_callback( unsigned int & element, bool insert_result, inserter_type & inserter) {}
             
             template<typename inserter_type>
-            void insert_callback( long & element, inserter_type & inserter) {}
+            void insert_callback( long & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( unsigned long & element, inserter_type & inserter) {}
+            void insert_callback( unsigned long & element, bool insert_result, inserter_type & inserter) {}
             
             template<typename inserter_type>
-            void insert_callback( float & element, inserter_type & inserter) {}
+            void insert_callback( float & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( double & element, inserter_type & inserter) {}
+            void insert_callback( double & element, bool insert_result, inserter_type & inserter) {}
             
             
             template<typename inserter_type>
-            void insert_callback( const bool & element, inserter_type & inserter) {}
+            void insert_callback( const bool & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( const char & element, inserter_type & inserter) {}
+            void insert_callback( const char & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( const unsigned char & element, inserter_type & inserter) {}
+            void insert_callback( const unsigned char & element, bool insert_result, inserter_type & inserter) {}
             
             template<typename inserter_type>
-            void insert_callback( const short & element, inserter_type & inserter) {}
+            void insert_callback( const short & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( const unsigned short & element, inserter_type & inserter) {}
+            void insert_callback( const unsigned short & element, bool insert_result, inserter_type & inserter) {}
             
             template<typename inserter_type>
-            void insert_callback( const int & element, inserter_type & inserter) {}
+            void insert_callback( const int & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( const unsigned int & element, inserter_type & inserter) {}
+            void insert_callback( const unsigned int & element, bool insert_result, inserter_type & inserter) {}
             
             template<typename inserter_type>
-            void insert_callback( const long & element, inserter_type & inserter) {}
+            void insert_callback( const long & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( const unsigned long & element, inserter_type & inserter) {}
+            void insert_callback( const unsigned long & element, bool insert_result, inserter_type & inserter) {}
             
             template<typename inserter_type>
-            void insert_callback( const float & element, inserter_type & inserter) {}
+            void insert_callback( const float & element, bool insert_result, inserter_type & inserter) {}
 
             template<typename inserter_type>
-            void insert_callback( const double & element, inserter_type & inserter) {}
+            void insert_callback( const double & element, bool insert_result, inserter_type & inserter) {}
+            
+            
+            
+            
+            
+            template<typename container_collection_element_type, typename id_type>
+            void set_id( container_collection_element_type & element, id_type id )
+            {
+                element.id(id);
+            }
+            
+            template<typename id_type> void set_id( bool & element, id_type id ) {}
+            template<typename id_type> void set_id( char & element, id_type id ) {}
+            template<typename id_type> void set_id( unsigned char & element, id_type id ) {}
+            template<typename id_type> void set_id( short & element, id_type id ) {}
+            template<typename id_type> void set_id( unsigned short & element, id_type id ) {}
+            template<typename id_type> void set_id( int & element, id_type id ) {}
+            template<typename id_type> void set_id( unsigned int & element, id_type id ) {}
+            template<typename id_type> void set_id( long & element, id_type id ) {}
+            template<typename id_type> void set_id( unsigned long & element, id_type id ) {}
+            template<typename id_type> void set_id( float & element, id_type id ) {}
+            template<typename id_type> void set_id( double & element, id_type id ) {}
+            
         }
     }
 }
