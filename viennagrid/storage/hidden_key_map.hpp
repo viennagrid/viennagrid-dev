e9a5a34853cf8cbff27caad1cadba93c524bcde0
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2012-10-05 10:58:57 +0200

    abstract hidden key map container class, used for boundary facets

---
+++ viennagrid/storage/hidden_key_map.hpp
@@ -0,0 +1,113 @@
+#ifndef VIENNAGRID_STORAGE_HIDDEN_KEY_MAP_HPP
+#define VIENNAGRID_STORAGE_HIDDEN_KEY_MAP_HPP
+
+#include <map>
+
+namespace viennagrid
+{
+    namespace storage
+    {
+
+        template<typename _key_type, typename _value_type>
+        class hidden_key_map
+        {
+            typedef std::map< _key_type, _value_type > container_type;
+            
+        public:
+            
+            typedef _key_type key_type;
+            typedef _value_type value_type;
+            typedef typename container_type::size_type size_type;
+            typedef value_type & reference;
+            typedef const value_type & const_reference;
+            typedef value_type * pointer;
+            typedef const value_type * const_pointer;
+            
+            
+            class iterator : public container_type::iterator
+            {
+                typedef typename container_type::iterator base;
+            public:
+                iterator(const base & foo) : base(foo) {}
+                
+                typedef hidden_key_map::value_type value_type;
+                typedef hidden_key_map::reference reference;
+                typedef hidden_key_map::const_reference const_reference;
+                typedef hidden_key_map::pointer pointer;
+                typedef hidden_key_map::const_pointer const_pointer;
+                
+                value_type & operator* () { return base::operator*().second; }
+                const value_type & operator* () const { return base::operator*().second; }
+            };
+            
+            class const_iterator : public container_type::const_iterator
+            {
+                typedef typename container_type::iterator base;
+            public:
+                const_iterator(const base & foo) : base(foo) {}
+                
+                typedef hidden_key_map::value_type value_type;
+                typedef hidden_key_map::const_reference reference;
+                typedef hidden_key_map::const_reference const_reference;
+                typedef hidden_key_map::const_pointer pointer;
+                typedef hidden_key_map::const_pointer const_pointer;
+                
+                const value_type & operator* () const { return base::operator*().second; }
+            };
+            
+            
+            iterator begin() { return iterator(container.begin()); }
+            iterator end() { return iterator(container.end()); }
+
+            const_iterator begin() const { return const_iterator(container.begin()); }
+            const_iterator end() const { return const_iterator(container.end()); }
+            
+            
+            std::pair<iterator, bool> insert( const value_type & element )
+            {
+                std::pair<typename container_type::iterator, bool> ret = container.insert( std::make_pair( key_type(element), element ) );
+                return std::make_pair( iterator(ret.first), ret.second );
+            }
+
+            
+            size_type size() const { return container.size(); }
+            bool empty() const { return container.empty(); }
+            
+        private:
+            container_type container;
+        };
+        
+        
+        
+        
+        
+        template<typename key_type_tag>
+        struct hidden_key_map_tag {};
+        
+        namespace result_of
+        {
+            template<typename element_tag, typename key_type_tag>
+            struct hidden_key_map_key_type_from_tag;
+            
+            
+            template<typename element_type, typename key_type_tag>
+            struct container_from_tag<element_type, hidden_key_map_tag<key_type_tag> >
+            {
+                typedef hidden_key_map< typename hidden_key_map_key_type_from_tag<element_type, key_type_tag>::type, element_type > type;
+            };
+        }
+        
+        namespace container
+        {
+            template<typename key_type, typename value_type>
+            std::pair<typename hidden_key_map<key_type, value_type>::iterator, bool>
+                insert( hidden_key_map<key_type, value_type> & container, const value_type & element )
+            {
+                return container.insert( element );
+            }
+
+        }
+    }
+}
+
+#endif
