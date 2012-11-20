fc1ab16839e904693625b8234ba7157beb907f71
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2012-11-20 11:36:36 +0100

    added wrapper for const container

--- viennagrid/storage/range.hpp
+++ viennagrid/storage/range.hpp
@@ -13,6 +13,8 @@ namespace viennagrid
         template<typename container_type>
         class container_range_wrapper
         {
+            friend class container_range_wrapper<const container_type>;
+            
         public:
             
             container_range_wrapper(container_type & _container) : container(_container) {}
@@ -65,6 +67,64 @@ namespace viennagrid
         private:
             
             container_type & container;
+        };
+        
+        template<typename container_type>
+        class container_range_wrapper<const container_type>
+        {
+        public:
+            
+            container_range_wrapper(const container_type & _container) : container(_container) {}
+            container_range_wrapper(const container_range_wrapper<container_type> & rhs) : container(rhs.container) {}
+            
+            typedef typename container_type::size_type size_type;
+            typedef typename container_type::value_type value_type;
+            
+            typedef typename container_type::reference reference;
+            typedef typename container_type::const_reference const_reference;
+            
+            typedef typename container_type::pointer pointer;
+            typedef typename container_type::const_pointer const_pointer;
+            
+            typedef typename container_type::const_iterator iterator;
+            typedef typename container_type::const_iterator const_iterator;
+            
+            typedef typename container_type::const_reverse_iterator reverse_iterator;
+            typedef typename container_type::const_reverse_iterator const_reverse_iterator;
+            
+            
+            
+            iterator begin() { return container.begin(); }
+            const_iterator begin() const { return container.begin(); }
+            iterator end() { return container.end(); }
+            const_iterator end() const { return container.end(); }
+            
+            reverse_iterator rbegin() { return container.rbegin(); }
+            const_reverse_iterator rbegin() const { return container.rbegin(); }
+            reverse_iterator rend() { return container.rend(); }
+            const_reverse_iterator rend() const { return container.rend(); }
+            
+            
+            
+            reference front() { return container.front(); }
+            const_reference front() const { return container.front(); }
+            reference back() { return container.back(); }
+            const_reference back() const { return container.back(); }
+            
+            
+            
+            reference operator[] (size_type index) { return container[index]; }
+            const_reference operator[] (size_type index) const { return container[index]; }
+            
+            
+            
+            bool empty() const { return container.empty(); }
+            size_type size() const { return container.size(); }
+            
+            
+        private:
+            
+            const container_type & container;
         };
         
         
