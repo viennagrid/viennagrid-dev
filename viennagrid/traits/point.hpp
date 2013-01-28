aac3a0976726252722c60d05ba6b3a5008a1ca06
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-01-28 17:44:12 +0100

    removed value_type, use result_of::coord_type instead

--- viennagrid/traits/point.hpp
+++ viennagrid/traits/point.hpp
@@ -76,19 +76,19 @@ namespace viennagrid
     }
     
     
-    /** @brief  Value Type retrieval for a point. Returns the numeric type of each coordinate entry */
-    template <typename PointType>
-    struct value_type
-    {
-        typedef typename PointType::value_type type;
-    };
-
-    /** @brief  Value Type retrieval for a point. Returns the numeric type of each coordinate entry. Specialization for a ViennaGrid point. */
-    template <typename CoordType, typename CoordinateSystem>
-    struct value_type< point_t<CoordType, CoordinateSystem> >
-    {
-      typedef CoordType    type;
-    };
+//     /** @brief  Value Type retrieval for a point. Returns the numeric type of each coordinate entry */
+//     template <typename PointType>
+//     struct value_type
+//     {
+//         typedef typename PointType::value_type type;
+//     };
+// 
+//     /** @brief  Value Type retrieval for a point. Returns the numeric type of each coordinate entry. Specialization for a ViennaGrid point. */
+//     template <typename CoordType, typename CoordinateSystem>
+//     struct value_type< point_t<CoordType, CoordinateSystem> >
+//     {
+//       typedef CoordType    type;
+//     };
     
   }
 } 
