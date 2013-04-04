38899c792f7dadaf11d915f0760da47c7f4c757b
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-04-04 17:59:23 +0200

    added fix for numeric problem in point_in_interval added intersection_result class for return value of intersect functions added callback functor support for intersection (is called when an intersection occurs) added point - ray intersection

--- viennagrid/algorithm/intersect.hpp
+++ viennagrid/algorithm/intersect.hpp
@@ -22,6 +22,7 @@
 #include "viennagrid/algorithm/numeric.hpp"
 #include "viennagrid/algorithm/inner_prod.hpp"
 #include "viennagrid/algorithm/norm.hpp"
+#include <boost/concept_check.hpp>
 
 namespace viennagrid 
 {
@@ -113,29 +114,40 @@ namespace viennagrid
             
             
             
-            template<typename numeric_type, typename first_tag_type, typename second_tag_type>
-            bool point_in_interval( numeric_type first, first_tag_type first_tag, numeric_type second, second_tag_type second_tag, numeric_type to_test )
+            template<typename numeric_type, typename first_tag_type, typename second_tag_type, typename numeric_config>
+            bool point_in_interval( numeric_type first, first_tag_type first_tag,
+                                    numeric_type second, second_tag_type second_tag,
+                                    numeric_type to_test,
+                                    numeric_config nc
+                                  )
             {
+                if ( numeric::is_equal(nc, first, to_test) )
+                    return is_closed(first_tag);
+                
+                if ( numeric::is_equal(nc, second, to_test) )
+                    return is_closed(second_tag);
+
                 return (first <= second) ?
                     (greater(first, to_test, first_tag) && less(second, to_test, second_tag)) :
                     (greater(second, to_test, second_tag) && less(first, to_test, first_tag));
             }
             
-            template<typename numeric_type, typename dual_tag>
-            bool point_in_interval( numeric_type first, numeric_type second, dual_tag tag, numeric_type to_test )
+            template<typename numeric_type, typename dual_tag, typename numeric_config>
+            bool point_in_interval( numeric_type first, numeric_type second, dual_tag tag, numeric_type to_test, numeric_config nc )
             {
-                return point_in_interval(first, first_tag(tag), second, second_tag(tag), to_test);
+                return point_in_interval(first, first_tag(tag), second, second_tag(tag), to_test, nc);
             }
             
             
-            template<typename numeric_type, typename interval_0_tag_type, typename interval_1_tag_type>
+            template<typename numeric_type, typename interval_0_tag_type, typename interval_1_tag_type, typename numeric_config>
             bool interval_intersect(numeric_type first_0, numeric_type second_0, interval_0_tag_type interval_0_tag,
-                                    numeric_type first_1, numeric_type second_1, interval_1_tag_type interval_1_tag )
+                                    numeric_type first_1, numeric_type second_1, interval_1_tag_type interval_1_tag,
+                                    numeric_config nc)
             {
-                return point_in_interval( first_0, intersect_tag(first_tag(interval_0_tag), first_tag(interval_1_tag)), second_0, intersect_tag(second_tag(interval_0_tag), first_tag(interval_1_tag)), first_1 ) ||
-                    point_in_interval( first_0, intersect_tag(first_tag(interval_0_tag), second_tag(interval_1_tag)), second_0, intersect_tag(second_tag(interval_0_tag), second_tag(interval_1_tag)), second_1 ) ||
-                    point_in_interval( first_1, intersect_tag(first_tag(interval_1_tag), first_tag(interval_0_tag)), second_1, intersect_tag(second_tag(interval_1_tag), first_tag(interval_0_tag)), first_0 ) ||
-                    point_in_interval( first_1, intersect_tag(first_tag(interval_1_tag), second_tag(interval_0_tag)), second_1, intersect_tag(second_tag(interval_1_tag), second_tag(interval_0_tag)), second_0 ) ||
+                return point_in_interval( first_0, intersect_tag(first_tag(interval_0_tag), first_tag(interval_1_tag)), second_0, intersect_tag(second_tag(interval_0_tag), first_tag(interval_1_tag)), first_1, nc ) ||
+                    point_in_interval( first_0, intersect_tag(first_tag(interval_0_tag), second_tag(interval_1_tag)), second_0, intersect_tag(second_tag(interval_0_tag), second_tag(interval_1_tag)), second_1, nc ) ||
+                    point_in_interval( first_1, intersect_tag(first_tag(interval_1_tag), first_tag(interval_0_tag)), second_1, intersect_tag(second_tag(interval_1_tag), first_tag(interval_0_tag)), first_0, nc ) ||
+                    point_in_interval( first_1, intersect_tag(first_tag(interval_1_tag), second_tag(interval_0_tag)), second_1, intersect_tag(second_tag(interval_1_tag), second_tag(interval_0_tag)), second_0, nc ) ||
                     ( (first_0 == first_1) && (second_0 == second_1) && (std::abs(second_0 - first_0) > 0) ) ||
                     ( (first_0 == second_1) && (second_0 == first_1) && (std::abs(second_0 - first_0) > 0) );
             }
@@ -143,10 +155,49 @@ namespace viennagrid
         }
         
         
-        template<typename point_type, typename numeric_config, typename line_tag>
-        bool point_line_intersect(point_type const & p,
-                                  point_type const & q0, point_type const & q1, line_tag tag,
-                                  numeric_config nc )
+        
+        class intersection_result
+        {
+            intersection_result() : dim_(0) {}
+            intersection_result(int intersection_dimension_) : dim_(intersection_dimension_ + 1) {}
+            
+        public:
+            
+            int dim() const { return dim_-1; }
+            
+            operator bool () const
+            {
+                return dim_;
+            }
+            
+            static intersection_result no_intersection()
+            {
+                return intersection_result();
+            }
+            
+            static intersection_result intersection_dimension(int intersection_dimension_)
+            {
+                assert(intersection_dimension_ >= 0);
+                return intersection_result(intersection_dimension_);
+            }
+            
+        private:
+            int dim_;
+        };
+        
+        
+        struct trivial_point_line_intersect_functor
+        {
+            template<typename point_type, typename line_tag>
+            void operator() (point_type const & p, point_type const & q0, point_type const & q1, line_tag tag) {}
+        };
+        
+        
+        
+        template<typename point_type, typename numeric_config, typename line_tag, typename intersection_functor_type>
+        intersection_result point_line_intersect(point_type const & p,
+                                       point_type const & q0, point_type const & q1, line_tag tag,
+                                       numeric_config nc, intersection_functor_type intersection_functor )
         {
             typedef typename viennagrid::result_of::coord_type< point_type >::type coord_type;
             typedef typename numeric::result_of::numeric_type<numeric_config, coord_type>::type inner_numeric_type;
@@ -159,25 +210,109 @@ namespace viennagrid
 //             std::cout << "  " << dir[0] << " " << lhs [0] << std::endl;
             
             if ( norm_1(dir) < relative_eps )
-                return false;
+                return intersection_result::no_intersection();
             
             
             if ( std::abs(dir[0] * lhs[1] - lhs[0] * dir[1]) < relative_eps )
             {
-                return (std::abs(dir[0]) < relative_eps) ?
-                    interval::point_in_interval(coord_type(0), dir[1], tag, lhs[1]) :
-                    interval::point_in_interval(coord_type(0), dir[0], tag, lhs[0]);
+                intersection_functor(p, q0, q1, tag);
+                
+                if ((std::abs(dir[0]) < relative_eps) ?
+                    interval::point_in_interval(coord_type(0), dir[1], tag, lhs[1], nc) :
+                    interval::point_in_interval(coord_type(0), dir[0], tag, lhs[0], nc))
+                {
+                    return intersection_result::intersection_dimension(0);
+                }
             }
 
-            return false;
+            return intersection_result::no_intersection();
+        }
+        
+        
+        template<typename point_type, typename numeric_config, typename line_tag>
+        intersection_result point_line_intersect(point_type const & p,
+                                       point_type const & q0, point_type const & q1, line_tag tag,
+                                       numeric_config nc)
+        {
+            trivial_point_line_intersect_functor f;
+            return point_line_intersect(p, q0, q1, tag, nc, f);
+        }
+
+        
+        
+        
+        struct trivial_point_ray_intersect_functor
+        {
+            template<typename point_type>
+            void operator() (point_type const & p, point_type const & q0, point_type const & q1) {}
+        };
+        
+        
+        template<typename point_type, typename numeric_config, typename intersection_functor_type>
+        intersection_result point_ray_intersect(point_type const & p,
+                                       point_type const & q0, point_type const & q1,
+                                       numeric_config nc, intersection_functor_type intersection_functor )
+        {
+            typedef typename viennagrid::result_of::coord_type< point_type >::type coord_type;
+            typedef typename numeric::result_of::numeric_type<numeric_config, coord_type>::type inner_numeric_type;
+            
+            point_type dir = q1 - q0;
+            point_type lhs = p - q0;
+            
+            inner_numeric_type relative_eps = numeric::relative_tolerance(nc, viennagrid::norm_1(dir) );
+            
+//             std::cout << "  " << dir[0] << " " << lhs [0] << std::endl;
+            
+            if ( norm_1(dir) < relative_eps )
+                return intersection_result::no_intersection();
+            
+            
+            if ( std::abs(dir[0] * lhs[1] - lhs[0] * dir[1]) < relative_eps )
+            {
+                intersection_functor(p, q0, q1);
+                return intersection_result::intersection_dimension(0);
+            }
+
+            return intersection_result::no_intersection();
+        }
+        
+        template<typename point_type, typename numeric_config>
+        intersection_result point_ray_intersect(point_type const & p,
+                                       point_type const & q0, point_type const & q1,
+                                       numeric_config nc)
+        {
+            trivial_point_ray_intersect_functor f;
+            return point_ray_intersect(p, q0, q1, nc, f);
         }
         
         
         
-        template <typename point_type, typename numeric_config, typename line1_tag, typename line2_tag>
-        bool line_line_intersect(point_type const & v0, point_type const & v1, line1_tag tag1, //endpoints of first line
-                                 point_type const & w0, point_type const & w1, line2_tag tag2, //endpoints of second line
-                                 numeric_config nc )
+        
+        struct trivial_line_line_intersect_point_intersection_functor
+        {
+            template<typename point_type, typename line1_tag, typename line2_tag, typename coord_type>
+            void operator() (point_type const & v0, point_type const & v1 , line1_tag tag1,
+                             point_type const & w0, point_type const & w1, line2_tag tag2,
+                             coord_type s, coord_type t, coord_type denominator) {}
+        };
+        
+        struct trivial_line_line_intersect_overlapping_lines_functor
+        {
+            template<typename point_type, typename line1_tag, typename line2_tag, typename coord_type>
+            void operator() (point_type const & v0, point_type const & v1 , line1_tag tag1,
+                             point_type const & w0, point_type const & w1, line2_tag tag2,
+                             coord_type first1, coord_type second1,
+                             coord_type first2, coord_type second2) {}
+        };
+        
+        
+        
+        template <typename point_type, typename numeric_config, typename line1_tag, typename line2_tag, typename point_intersection_functor_type, typename overlapping_lines_functor_type>
+        intersection_result line_line_intersect(
+                                point_type const & v0, point_type const & v1, line1_tag tag1, //endpoints of first line
+                                point_type const & w0, point_type const & w1, line2_tag tag2, //endpoints of second line
+                                numeric_config nc,
+                                point_intersection_functor_type point_intersection_functor, overlapping_lines_functor_type overlapping_lines_functor)
         {
             //typedef point_t<CoordType, CoordinateSystem>  PointType;
             typedef typename viennagrid::result_of::coord_type< point_type >::type coord_type;
@@ -212,10 +347,16 @@ namespace viennagrid
                     coord_type w_first = lhs_v[index];
                     coord_type w_second = w1[index] - v0[index];
                         
-                    return interval::interval_intersect( coord_type(0), dir_v[index], tag1, w_first, w_second, tag2 );
+                    if (interval::interval_intersect( coord_type(0), dir_v[index], tag1, w_first, w_second, tag2, nc ))
+                    {
+                        overlapping_lines_functor(v0, v1, tag1, w0, w1, tag2, coord_type(0), dir_v[index], w_first, w_second);
+                        return intersection_result::intersection_dimension(1);
+                    }
+                    else
+                        return intersection_result::no_intersection();
                 }
                 else
-                    return false;
+                    return intersection_result::no_intersection();
             }
 
             //Lines are not parallel: Compute minimizers s, t:
@@ -230,7 +371,25 @@ namespace viennagrid
             coord_type s = (v_in_w * w_in_dir_distance - w_in_w * v_in_dir_distance);
             coord_type t = (v_in_v * w_in_dir_distance - v_in_w * v_in_dir_distance);
             
-            return point_in_interval(coord_type(0), denominator, tag1, s) && point_in_interval(coord_type(0), denominator, tag2, t);
+            if (point_in_interval(coord_type(0), denominator, tag1, s, nc) && point_in_interval(coord_type(0), denominator, tag2, t, nc))
+            {
+                point_intersection_functor(v0, v1, tag1, w0, w1, tag2, s, t, denominator);
+                return intersection_result::intersection_dimension(0);
+            }
+            else
+                return intersection_result::no_intersection();
+            
+//             return point_in_interval(coord_type(0), denominator, tag1, s) && point_in_interval(coord_type(0), denominator, tag2, t);
+        }
+        
+        template <typename point_type, typename numeric_config, typename line1_tag, typename line2_tag>
+        intersection_result line_line_intersect(point_type const & v0, point_type const & v1, line1_tag tag1, //endpoints of first line
+                                 point_type const & w0, point_type const & w1, line2_tag tag2, //endpoints of second line
+                                 numeric_config nc)
+        {
+            trivial_line_line_intersect_point_intersection_functor f1;
+            trivial_line_line_intersect_overlapping_lines_functor f2;
+            return line_line_intersect(v0, v1, tag1, w0, w1, tag2, nc, f1, f2);
         }
         
         
