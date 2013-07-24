38f9cfcdc5a62b564589a31cceee644a47dce9cd
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-07-24 18:01:22 +0200

    adapted to new interface, renamed copy_domain -> copy_cells

--- viennagrid/domain/domain_operations.hpp
+++ viennagrid/domain/domain_operations.hpp
@@ -24,64 +24,67 @@
 namespace viennagrid
 {
 
-    template<typename src_domain_type, typename src_segment_container_type, typename dst_domain_type, typename dst_segment_container_type>
-    void copy_domain(
-            src_domain_type const & src_domain, src_segment_container_type const & src_segments,
-            dst_domain_type & dst_domain, dst_segment_container_type & dst_segments )
-    {
-        typedef typename src_segment_container_type::value_type src_segment_type;
-        typedef typename dst_segment_container_type::value_type dst_segment_type;
+  template<typename SrcDomainT, typename SrcSegmentationT, typename DstDomainT, typename DstSegmentationT>
+  void copy_cells(SrcDomainT const & src_domain,  SrcSegmentationT const & src_segmentation,
+                  DstDomainT & dst_domain,        DstSegmentationT & dst_segmentation )
+  {
+  //     typedef typename src_segment_container_type::value_type src_segment_type;
+  //     typedef typename dst_segment_container_type::value_type dst_segment_type;
+      typedef typename viennagrid::result_of::segment<SrcSegmentationT>::type SrcSegmentType;
+      typedef typename viennagrid::result_of::segment<DstSegmentationT>::type DstSegmentType;
+      
+      typedef typename viennagrid::result_of::cell_tag<SrcDomainT>::type  SrcCellTag;
+      typedef typename viennagrid::result_of::cell<SrcDomainT>::type      SrcCellType;
+      
+      typedef typename viennagrid::result_of::const_vertex_range<SrcDomainT>::type    SrcVertexRangeType;
+      typedef typename viennagrid::result_of::iterator<SrcVertexRangeType>::type      SrcVertexRangeIterator;
+      
+      typedef typename viennagrid::result_of::const_cell_range<SrcSegmentType>::type  SrcCellRangeType;
+      typedef typename viennagrid::result_of::iterator<SrcCellRangeType>::type        SrcCellRangeIterator;
+      
+      typedef typename viennagrid::result_of::const_vertex_handle<SrcDomainT>::type   SrcConstVertexHandle;
+      typedef typename viennagrid::result_of::vertex_handle<DstDomainT>::type         DstVertexHandleType;
+      
+      if (&src_domain == &dst_domain)
+          return;
+      
+      dst_domain.clear();
+      dst_segmentation.clear();
+      
+      std::map<SrcConstVertexHandle, DstVertexHandleType> vertex_handle_map;
+      
+      SrcVertexRangeType vertices = viennagrid::elements( src_domain );
+  //     std::cout << "Copy: vertex count = " << vertices.size() << std::endl;
+      for (SrcVertexRangeIterator it = vertices.begin(); it != vertices.end(); ++it)
+          vertex_handle_map[it.handle()] = viennagrid::make_vertex( dst_domain, viennagrid::point(src_domain, *it) );
+      
 
-        typedef typename viennagrid::result_of::cell_tag<src_domain_type>::type src_cell_tag;
-        typedef typename viennagrid::result_of::cell_type<src_domain_type>::type src_cell_type;
-
-        typedef typename viennagrid::result_of::const_vertex_range<src_domain_type>::type src_vertex_range_type;
-        typedef typename viennagrid::result_of::iterator<src_vertex_range_type>::type src_vertex_range_iterator;
-
-        typedef typename viennagrid::result_of::const_cell_range<src_segment_type>::type src_cell_range_type;
-        typedef typename viennagrid::result_of::iterator<src_cell_range_type>::type src_cell_range_itertor;
-
-        typedef typename viennagrid::result_of::const_vertex_handle<src_domain_type>::type src_const_vertex_handle;
-        typedef typename viennagrid::result_of::vertex_handle<dst_domain_type>::type dst_vertex_handle;
-
-        if (&src_domain == &dst_domain)
-            return;
-
-        viennagrid::clear_domain(dst_domain);
-        dst_segments.clear();
-
-        std::map<src_const_vertex_handle, dst_vertex_handle> vertex_handle_map;
-
-        src_vertex_range_type vertices = viennagrid::elements( src_domain );
-        for (src_vertex_range_iterator it = vertices.begin(); it != vertices.end(); ++it)
-            vertex_handle_map[it.handle()] = viennagrid::create_vertex( dst_domain, viennagrid::point(src_domain, *it) );
-
-
-
-        for (typename src_segment_container_type::const_iterator seg_it = src_segments.begin(); seg_it != src_segments.end(); ++seg_it)
-        {
-            dst_segments.push_back( viennagrid::create_view<dst_segment_type>(dst_domain) );
-            dst_segment_type & dst_segment = dst_segments.back();
-
-            src_cell_range_type cells = viennagrid::elements( *seg_it );
-            for (src_cell_range_itertor it = cells.begin(); it != cells.end(); ++it)
-            {
-                src_cell_type const & cell = *it;
-
-                std::deque<dst_vertex_handle> vertex_handles;
-
-                typedef typename viennagrid::result_of::const_vertex_range<src_cell_type>::type src_vertex_on_src_cell_range_type;
-                typedef typename viennagrid::result_of::iterator<src_vertex_on_src_cell_range_type>::type src_vertex_on_src_cell_range_iterator;
-
-                src_vertex_on_src_cell_range_type cell_vertices = viennagrid::elements(cell);
-                for (src_vertex_on_src_cell_range_iterator jt = cell_vertices.begin(); jt != cell_vertices.end(); ++jt)
-                    vertex_handles.push_back( vertex_handle_map[jt.handle()] );
-
-                typedef typename viennagrid::result_of::cell_type<dst_domain_type>::type dst_cell_type;
-                viennagrid::create_element<dst_cell_type>( dst_segment, vertex_handles.begin(), vertex_handles.end() );
-            }
-        }
-    }
+      
+      for (typename SrcSegmentationT::const_iterator seg_it = src_segmentation.begin(); seg_it != src_segmentation.end(); ++seg_it)
+      {
+  //         dst_segments.push_back( viennagrid::make_view<dst_segment_type>(dst_domain) );
+          DstSegmentType & dst_segment = dst_segmentation.get_make_segment( seg_it->id() );
+          
+          SrcCellRangeType cells = viennagrid::elements( *seg_it );
+  //         std::cout << "Copy: cell count = " << cells.size() << std::endl;
+          for (SrcCellRangeIterator it = cells.begin(); it != cells.end(); ++it)
+          {
+              SrcCellType const & cell = *it;
+              
+              std::deque<DstVertexHandleType> vertex_handles;
+              
+              typedef typename viennagrid::result_of::const_vertex_range<SrcCellType>::type SrcVertexOnSrcCellRangeType;
+              typedef typename viennagrid::result_of::iterator<SrcVertexOnSrcCellRangeType>::type SrcVertexOnSrcCellRangeIterator;
+              
+              SrcVertexOnSrcCellRangeType cell_vertices = viennagrid::elements(cell);
+              for (SrcVertexOnSrcCellRangeIterator jt = cell_vertices.begin(); jt != cell_vertices.end(); ++jt)
+                  vertex_handles.push_back( vertex_handle_map[jt.handle()] );
+              
+              typedef typename viennagrid::result_of::cell<DstDomainT>::type DstCellType;
+              viennagrid::make_element<DstCellType>( dst_segment, vertex_handles.begin(), vertex_handles.end() );
+          }
+      }
+  }
 
 }
 
