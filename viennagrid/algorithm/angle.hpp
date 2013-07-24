1b35adc06a2f900908794fa73fa66df0b5951c09
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-07-24 13:34:37 +0200

    adapted to new interface (coord_type -> coord)

--- viennagrid/algorithm/angle.hpp
+++ viennagrid/algorithm/angle.hpp
@@ -9,16 +9,16 @@ namespace viennagrid
 {
 
     template<typename point_type>
-    typename viennagrid::result_of::coord_type<point_type>::type spanned_angle( point_type const & v0, point_type const & v1 )
+    typename viennagrid::result_of::coord<point_type>::type spanned_angle( point_type const & v0, point_type const & v1 )
     {
         return std::acos( viennagrid::inner_prod(v0, v1) / (viennagrid::norm_2(v0) * viennagrid::norm_2(v1)) );
     }
 
     // http://en.wikipedia.org/wiki/Solid_angle#Tetrahedron
     template<typename point_type>
-    typename viennagrid::result_of::coord_type<point_type>::type spanned_angle( point_type const & a, point_type const & b, point_type const & c )
+    typename viennagrid::result_of::coord<point_type>::type spanned_angle( point_type const & a, point_type const & b, point_type const & c )
     {
-        typedef typename viennagrid::result_of::coord_type<point_type>::type coord_type;
+        typedef typename viennagrid::result_of::coord<point_type>::type coord_type;
 
         coord_type det = viennagrid::geometry::determinant(a, b, c);
 
@@ -35,8 +35,8 @@ namespace viennagrid
 
 
     template<typename point_type>
-    typename viennagrid::result_of::coord_type<point_type>::type dihedral_angle( point_type const & p0, point_type const & p1, point_type const & p2,
-                                                                                 point_type const & q0, point_type const & q1, point_type const & q2)
+    typename viennagrid::result_of::coord<point_type>::type dihedral_angle( point_type const & p0, point_type const & p1, point_type const & p2,
+                                                                            point_type const & q0, point_type const & q1, point_type const & q2)
     {
         point_type np = viennagrid::cross_prod( p1-p0, p2-p0 );
         point_type nq = viennagrid::cross_prod( q1-q0, q2-q0 );
@@ -48,7 +48,7 @@ namespace viennagrid
 
 
     template<typename point_type>
-    typename viennagrid::result_of::coord_type<point_type>::type angle( point_type const & origin, point_type const & p0, point_type const & p1 )
+    typename viennagrid::result_of::coord<point_type>::type angle( point_type const & origin, point_type const & p0, point_type const & p1 )
     {
         point_type v0 = p0 - origin;
         point_type v1 = p1 - origin;
@@ -57,7 +57,7 @@ namespace viennagrid
     }
 
     template<typename point_type>
-    typename viennagrid::result_of::coord_type<point_type>::type angle( point_type const & origin, point_type const & p0, point_type const & p1, point_type const & p2 )
+    typename viennagrid::result_of::coord<point_type>::type angle( point_type const & origin, point_type const & p0, point_type const & p1, point_type const & p2 )
     {
         point_type v0 = p0 - origin;
         point_type v1 = p1 - origin;
@@ -68,7 +68,7 @@ namespace viennagrid
 
     // same as above, http://en.wikipedia.org/wiki/Solid_angle#Tetrahedron
     template<typename point_type>
-    typename viennagrid::result_of::coord_type<point_type>::type angle2( point_type const & origin, point_type const & a, point_type const & b, point_type const & c )
+    typename viennagrid::result_of::coord<point_type>::type angle2( point_type const & origin, point_type const & a, point_type const & b, point_type const & c )
     {
         double da_ab = dihedral_angle( origin, a, c, origin, b, c );
         double da_ac = dihedral_angle( origin, a, b, origin, c, b );
