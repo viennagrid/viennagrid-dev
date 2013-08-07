d54ddeecba69c56cb3c26e977b9643ea018f5bbd
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-08-07 17:57:30 +0200

    Better documentation

--- examples/tutorial/domain_setup.cpp
+++ examples/tutorial/domain_setup.cpp
@@ -24,42 +24,8 @@
 #include "viennagrid/forwards.hpp"
 #include "viennagrid/domain/domain.hpp"
 #include "viennagrid/domain/element_creation.hpp"
-#include "viennagrid/config/domain_config.hpp"
+#include "viennagrid/config/default_configs.hpp"
 #include "viennagrid/point.hpp"
-
-
-template <typename CellType, typename DomainType, typename SegmentType>
-void setup_cell(DomainType & domain,
-                SegmentType & segment,
-                std::size_t id0,
-                std::size_t id1,
-                std::size_t id2)
-{
-  typedef typename viennagrid::result_of::handle<DomainType, viennagrid::vertex_tag>::type       VertexHandleType;
-  
-  viennagrid::storage::static_array<VertexHandleType, 3> vertices;
-  vertices[0] = viennagrid::elements<viennagrid::vertex_tag>(domain).handle_at( id0 );
-  vertices[1] = viennagrid::elements<viennagrid::vertex_tag>(domain).handle_at( id1 );
-  vertices[2] = viennagrid::elements<viennagrid::vertex_tag>(domain).handle_at( id2 );
-  
-  viennagrid::make_element<CellType>(segment, vertices.begin(), vertices.end());
-}
-
-
-
-
-class my_domain_config
-{
-  private:
-    //typedef viennagrid::storage::pointer_handle_tag handle_tag;
-    //typedef viennagrid::storage::iterator_handle_tag handle_tag;
-    typedef viennagrid::storage::id_handle_tag          handle_tag;
-    
-  public:
-    
-    typedef viennagrid::point_t<double, viennagrid::cartesian_cs<2> > PointType;
-    typedef viennagrid::config::result_of::full_domain_config< viennagrid::triangle_tag, PointType, viennagrid::storage::pointer_handle_tag >::type type;
-};
 
 
 //
@@ -80,23 +46,17 @@ int main()
   // Define the necessary types:
   //
   
-  typedef my_domain_config::PointType PointType;
+  typedef viennagrid::triangular_2d_domain                  DomainType;
+  typedef viennagrid::triangular_2d_segmentation            SegmentationType;
+  typedef viennagrid::triangular_2d_segment                 SegmentType;
   
-  //typedef viennagrid::config::triangular_2d                       ConfigType;
-  //typedef viennagrid::result_of::domain<ConfigType>::type         Domain;
-  //typedef viennagrid::result_of::segment<ConfigType>::type        Segment;
+  typedef viennagrid::result_of::point<DomainType>::type            PointType;
+  typedef viennagrid::result_of::vertex<DomainType>::type           VertexType;
+  typedef viennagrid::result_of::vertex_handle<DomainType>::type    VertexHandleType;
   
-  typedef viennagrid::domain_t< my_domain_config > Domain;  
-  typedef viennagrid::result_of::domain_view<Domain>::type Segment;
-  typedef viennagrid::triangle_tag                                    CellTag;
-  
-  //typedef viennagrid::result_of::point<ConfigType>::type          PointType;
-  typedef viennagrid::result_of::element<Domain, viennagrid::vertex_tag>::type       VertexType;
-  typedef viennagrid::result_of::handle<Domain, viennagrid::vertex_tag>::type       VertexHandleType;
-  typedef viennagrid::result_of::element<Domain, CellTag>::type   CellType;
-
-  typedef viennagrid::result_of::element_range<Segment, CellTag>::type    CellRange;
-  typedef viennagrid::result_of::iterator<CellRange>::type                             CellIterator;
+  typedef viennagrid::result_of::cell<DomainType>::type             CellType;
+  typedef viennagrid::result_of::cell_range<SegmentType>::type      CellRange;
+  typedef viennagrid::result_of::iterator<CellRange>::type          CellIterator;
 
   std::cout << "-------------------------------------------------------------- " << std::endl;
   std::cout << "-- ViennaGrid tutorial: Setup of a domain with two segments -- " << std::endl;
@@ -104,65 +64,52 @@ int main()
   std::cout << std::endl;
 
   //
-  // Step 1: Instantiate the domain and create two segments:
+  // Step 1: Instantiate the domain and the segmentation and create 2 segments:
   //
-  Domain domain;
-//   std::vector<Segment> segments;
-//   
-//   segments.resize(2);
-//   segments[0] = viennagrid::make_view<Segment>(domain);
-//   segments[1] = viennagrid::make_view<Segment>(domain);
-//   
-  
-  //domain.segments().resize(2);
-  Segment seg0 = viennagrid::make_view(domain);
-  Segment seg1 = viennagrid::make_view(domain);
+  DomainType domain;
+  SegmentationType segmentation(domain);
+
+  SegmentType seg0 = segmentation.make_segment();
+  SegmentType seg1 = segmentation.make_segment();
   
   //
   // Step 2: Add vertices to the domain. 
   //         Note that vertices with IDs are enumerated in the order they are pushed to the domain.
   //
-  VertexHandleType vh0 = viennagrid::make_vertex(domain, PointType(0,0));
-  VertexHandleType vh1 = viennagrid::make_vertex(domain, PointType(1,0));
+  VertexHandleType vh0 = viennagrid::make_vertex(domain, PointType(0,0)); // id = 0
+  VertexHandleType vh1 = viennagrid::make_vertex(domain, PointType(1,0)); // id = 1
   VertexHandleType vh2 = viennagrid::make_vertex(domain, PointType(2,0));
   VertexHandleType vh3 = viennagrid::make_vertex(domain, PointType(2,1));
   VertexHandleType vh4 = viennagrid::make_vertex(domain, PointType(1,1));
-  VertexHandleType vh5 = viennagrid::make_vertex(domain, PointType(0,1));
+  VertexHandleType vh5 = viennagrid::make_vertex(domain, PointType(0,1)); // id = 5
 
-  
   //
   // Step 3: Fill the two segments with cells. 
-  //         To do so, each cell must be linked with the defining vertices from the domain (not v0, v1, ...!)
+  //         To do so, each cell must be linked with the defining vertex handles from the domain
   //
   
+  // First triangle, use vertex handles
   viennagrid::storage::static_array<VertexHandleType, 3> vertices;
-  
-  
-  //CellType cell;
-  //VertexType * cell_vertices[3]; //holds pointers to the respective vertices in the domain
-
-  // First triangle: (do not use v0, v1, etc. for vertex setup!)
   vertices[0] = vh0;
   vertices[1] = vh1;
   vertices[2] = vh5;
-  //Note that vertices are rearranged internally if they are not supplied in mathematically positive order.
+  // Note that vertices are rearranged internally if they are not supplied in mathematically positive order.
   
-  viennagrid::make_element<CellType>(seg0, vertices.begin(), vertices.end()); //copies 'cell' to the domain. 'cell' can be reused for setting up the other cells.
+  viennagrid::make_element<CellType>(seg0, vertices.begin(), vertices.end()); // creates an element with vertex handles
   
   
   // Second triangle:
-  setup_cell<CellType>(domain, seg0, 1, 4, 5);  //use the shortcut function defined at the beginning of this tutorial
+  viennagrid::make_triangle(seg0, vh1, vh4, vh5);  //use the shortcut function
 
   // Third triangle:
-  setup_cell<CellType>(domain, seg1, 1, 2, 4); // Note that we push to 'seg1' now.
+  viennagrid::make_triangle(seg1, vh1, vh2, vh4); // Note that we create in 'seg1' now.
 
   // Fourth triangle:
-//   setup_cell<CellType>(domain, seg1, 2, 3, 4);
-  viennagrid::make_triangle( seg1, vh2, vh3, vh4 );
+  viennagrid::make_triangle(seg1, vh2, vh3, vh4 );
 
   //
   // That's it. The domain consisting of two segments is now set up.
-  // If no segments are required, one can also directly write domain.push_back(cell);
+  // If no segments are required, one can also directly write viennagrid::make_triangle(domain, ...);
   //
   
   //
@@ -170,7 +117,7 @@ int main()
   //
   
   std::cout << "Cells in segment 0:" << std::endl;
-  CellRange cells_seg0 = viennagrid::elements<CellType>(seg0);
+  CellRange cells_seg0 = viennagrid::elements(seg0);
   for (CellIterator cit0 = cells_seg0.begin();
                     cit0 != cells_seg0.end();
                   ++cit0)
@@ -180,7 +127,7 @@ int main()
   std::cout << std::endl;
   
   std::cout << "Cells in segment 1:" << std::endl;
-  CellRange cells_seg1 = viennagrid::elements<CellType>(seg1);
+  CellRange cells_seg1 = viennagrid::elements(seg1);
   for (CellIterator cit1 = cells_seg1.begin();
                     cit1 != cells_seg1.end();
                   ++cit1)
--- examples/tutorial/neighbour_iteration.cpp
+++ examples/tutorial/neighbour_iteration.cpp
@@ -40,117 +40,130 @@ int main()
 {
     
     //
-    // typedefing and setting up the topological domain
+    // typedefing and setting up the domain
     //
     
-    typedef viennagrid::triangular_2d_domain domain_type;
-    domain_type domain;
+    typedef viennagrid::triangular_2d_domain DomainType;
+    DomainType domain;
     
     //
     // typedefs for the element types
     //    
      
-    typedef viennagrid::result_of::element<domain_type, viennagrid::vertex_tag>::type vertex_type;
-    typedef viennagrid::result_of::handle<domain_type, viennagrid::vertex_tag>::type vertex_handle_type;
+    typedef viennagrid::result_of::vertex_handle<DomainType>::type        VertexHandleType;
     
-    typedef viennagrid::result_of::element<domain_type, viennagrid::line_tag>::type line_type;
-    typedef viennagrid::result_of::element<domain_type, viennagrid::triangle_tag>::type triangle_type;
-    typedef viennagrid::result_of::handle<domain_type, viennagrid::triangle_tag>::type triangle_handle_type;
+    typedef viennagrid::result_of::triangle<DomainType>::type             TriangleType;
+    typedef viennagrid::result_of::triangle_handle<DomainType>::type      TriangleHandleType;
     
 
     //
     // Adding a tetrahedron
     //
 
-    // creates four vertices within the domain, vh is short vor vertex handle
-    vertex_handle_type vh0 = viennagrid::make_vertex( domain );
-    vertex_handle_type vh1 = viennagrid::make_vertex( domain );
-    vertex_handle_type vh2 = viennagrid::make_vertex( domain );
-    vertex_handle_type vh3 = viennagrid::make_vertex( domain );
-    vertex_handle_type vh4 = viennagrid::make_vertex( domain );
-    vertex_handle_type vh5 = viennagrid::make_vertex( domain );
-    vertex_handle_type vh6 = viennagrid::make_vertex( domain );
-    vertex_handle_type vh7 = viennagrid::make_vertex( domain );
-    vertex_handle_type vh8 = viennagrid::make_vertex( domain );
+    // Creating nine vertices within the domain, vh is short vor vertex handle
+    // In this example we don't need geometric information -> no points
+    //
+    //  0 --- 1 --- 2
+    //  |   / | \   |
+    //  |  /  |  \  |
+    //  | /   |   \ |
+    //  3 --- 4 --- 5
+    //  | \   |   / |
+    //  |  \  |  /  |
+    //  |   | | /   |
+    //  6 --- 7 --- 8
+    //
+        
+    VertexHandleType vh0 = viennagrid::make_vertex( domain );
+    VertexHandleType vh1 = viennagrid::make_vertex( domain );
+    VertexHandleType vh2 = viennagrid::make_vertex( domain );
+    VertexHandleType vh3 = viennagrid::make_vertex( domain );
+    VertexHandleType vh4 = viennagrid::make_vertex( domain );
+    VertexHandleType vh5 = viennagrid::make_vertex( domain );
+    VertexHandleType vh6 = viennagrid::make_vertex( domain );
+    VertexHandleType vh7 = viennagrid::make_vertex( domain );
+    VertexHandleType vh8 = viennagrid::make_vertex( domain );
        
-    // creates the tetrahedron within the domain, all boundary cell generation is done here implicit
-    triangle_handle_type th0 = viennagrid::make_triangle( domain, vh0, vh1, vh3 );
-    triangle_handle_type th1 = viennagrid::make_triangle( domain, vh1, vh4, vh3 );
-    triangle_handle_type th2 = viennagrid::make_triangle( domain, vh1, vh5, vh4 );
-    triangle_handle_type th3 = viennagrid::make_triangle( domain, vh1, vh2, vh5 );
-    triangle_handle_type th4 = viennagrid::make_triangle( domain, vh3, vh7, vh6 );
-    triangle_handle_type th5 = viennagrid::make_triangle( domain, vh3, vh4, vh7 );
-    triangle_handle_type th6 = viennagrid::make_triangle( domain, vh4, vh5, vh7 );
-    triangle_handle_type th7 = viennagrid::make_triangle( domain, vh5, vh8, vh7 );
+    // Creating the triangles within the domain, all boundary cell generation is done here implicit
+    //
+    //  X --- X --- X
+    //  | 0 / | \ 3 |
+    //  |  /  |  \  |
+    //  | / 1 | 2 \ |
+    //  X --- X --- X
+    //  | \ 5 | 6 / |
+    //  |  \  |  /  |
+    //  | 4 | | / 7 |
+    //  X --- X --- X
+    //
     
-    std::cout << "th0: " << th0 << std::endl;
-    std::cout << "th1: " << th1 << std::endl;
-    std::cout << "th2: " << th2 << std::endl;
-    std::cout << "th3: " << th3 << std::endl;
-    std::cout << "th4: " << th4 << std::endl;
-    std::cout << "th5: " << th5 << std::endl;
-    std::cout << "th6: " << th6 << std::endl;
-    std::cout << "th7: " << th7 << std::endl;
-
-
-
-    viennagrid::create_neighbour_information< viennagrid::triangle_tag, viennagrid::vertex_tag >( domain );
+    TriangleHandleType th0 = viennagrid::make_triangle( domain, vh0, vh1, vh3 );
+    TriangleHandleType th1 = viennagrid::make_triangle( domain, vh1, vh4, vh3 );
+    TriangleHandleType th2 = viennagrid::make_triangle( domain, vh1, vh5, vh4 );
+    TriangleHandleType th3 = viennagrid::make_triangle( domain, vh1, vh2, vh5 );
+    TriangleHandleType th4 = viennagrid::make_triangle( domain, vh3, vh7, vh6 );
+    TriangleHandleType th5 = viennagrid::make_triangle( domain, vh3, vh4, vh7 );
+    TriangleHandleType th6 = viennagrid::make_triangle( domain, vh4, vh5, vh7 );
+    TriangleHandleType th7 = viennagrid::make_triangle( domain, vh5, vh8, vh7 );
     
+    
+
+
+
 
     {
-        typedef viennagrid::result_of::neighbour_range<domain_type, viennagrid::triangle_tag, viennagrid::vertex_tag>::type neighbour_range_type;
-        
-        neighbour_range_type neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::vertex_tag>(domain, th1);
-        cout << "All triangles neighbouring th1 using vertices" << endl;
-        std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<triangle_type>(cout, "\n") );
-        cout << endl;
-        
-        neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::vertex_tag>(domain, th1);
-        cout << "All triangles neighbouring th1 using vertices" << endl;
-        std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<triangle_type>(cout, "\n") );
-        cout << endl;
-        
-        neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::vertex_tag>(domain, th0);
-        cout << "All triangles neighbouring th0 using vertices" << endl;
-        std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<triangle_type>(cout, "\n") );
-        cout << endl;
+      
+      typedef viennagrid::result_of::neighbour_range<DomainType, viennagrid::triangle_tag, viennagrid::vertex_tag>::type neighbour_range_type;
+      
+      // we now want to iterate over all neighbour triangles of triangle 1 which share a common vertex. These should be the triangles 0, 2, 3, 4, 5 and 6
+      neighbour_range_type neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::vertex_tag>(domain, th1);
+      cout << "All triangles neighbouring th1 using vertices (should be triangle 0, 2, 3, 4, 5 and 6)" << endl;
+      std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<TriangleType>(cout, "\n") );
+      cout << endl;
+      
+      // we now want to iterate over all neighbour triangles of triangle 0 which share a common vertex. These should be the triangles 1, 2, 3, 4 and 5
+      neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::vertex_tag>(domain, th0);
+      cout << "All triangles neighbouring th0 using vertices (should be triangle 1, 2, 3, 4 and 5)" << endl;
+      std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<TriangleType>(cout, "\n") );
+      cout << endl;
     }
     
-    {
-        typedef viennagrid::result_of::neighbour_range<domain_type, viennagrid::triangle_tag, viennagrid::line_tag>::type neighbour_range_type;
-        
-        neighbour_range_type neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::line_tag>(domain, th1);
-        cout << "All triangles neighbouring th1 using lines" << endl;
-        std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<triangle_type>(cout, "\n") );
-        cout << endl;
-        
-        neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::line_tag>(domain, th0);
-        cout << "All triangles neighbouring th0 using lines" << endl;
-        std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<triangle_type>(cout, "\n") );
-        cout << endl;
-    }
     
     {
-        cout << "CONST" << endl;
+        // now we do the same but with a constant domain
         
-        const domain_type & cdomain = domain;
-        typedef viennagrid::result_of::const_neighbour_range<domain_type, viennagrid::triangle_tag, viennagrid::vertex_tag>::type const_neighbour_range_type;
+        const DomainType & cdomain = domain;
+        typedef viennagrid::result_of::const_neighbour_range<DomainType, viennagrid::triangle_tag, viennagrid::vertex_tag>::type const_neighbour_range_type;
 
         const_neighbour_range_type const_neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::vertex_tag>(cdomain, th1);
-        cout << "All triangles neighbouring th1" << endl;
-        std::copy( const_neighbour_range.begin(), const_neighbour_range.end(), std::ostream_iterator<triangle_type>(cout, "\n") );
-        cout << endl;
-        
-        const_neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::vertex_tag>(cdomain, th1);
-        cout << "All triangles neighbouring th1" << endl;
-        std::copy( const_neighbour_range.begin(), const_neighbour_range.end(), std::ostream_iterator<triangle_type>(cout, "\n") );
+        cout << "All triangles neighbouring th1 using vertices and const ranges (should be triangle 0, 2, 3, 4, 5 and 6)" << endl;
+        std::copy( const_neighbour_range.begin(), const_neighbour_range.end(), std::ostream_iterator<TriangleType>(cout, "\n") );
         cout << endl;
         
         const_neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::vertex_tag>(cdomain, th0);
-        cout << "All triangles neighbouring th0" << endl;
-        std::copy( const_neighbour_range.begin(), const_neighbour_range.end(), std::ostream_iterator<triangle_type>(cout, "\n") );
+        cout << "All triangles neighbouring th0 using vertices and const ranges  (should be triangle 1, 2, 3, 4 and 5)" << endl;
+        std::copy( const_neighbour_range.begin(), const_neighbour_range.end(), std::ostream_iterator<TriangleType>(cout, "\n") );
         cout << endl;
     }
+    
+    
+    {
+        typedef viennagrid::result_of::neighbour_range<DomainType, viennagrid::triangle_tag, viennagrid::line_tag>::type neighbour_range_type;
+        
+        // we now want to iterate over all neighbour triangles of triangle 1 which share a common edge. These should be the triangles 0, 2 and 5
+        neighbour_range_type neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::line_tag>(domain, th1);
+        cout << "All triangles neighbouring th1 using lines (should be triangle 0, 2 and 5)" << endl;
+        std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<TriangleType>(cout, "\n") );
+        cout << endl;
+        
+        // we now want to iterate over all neighbour triangles of triangle 0 which share a common edge. This should only be the triangle 1
+        neighbour_range = viennagrid::neighbour_elements<viennagrid::triangle_tag, viennagrid::line_tag>(domain, th0);
+        cout << "All triangles neighbouring th0 using lines (should only be triangle 1)" << endl;
+        std::copy( neighbour_range.begin(), neighbour_range.end(), std::ostream_iterator<TriangleType>(cout, "\n") );
+        cout << endl;
+    }
+    
+
 
 
     return 0;
