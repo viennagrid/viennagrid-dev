fe3422d2b6cc033b53a77642c614ffb625f6c98d
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2013-10-08 11:27:38 +0200

    some more example for erasing element from segment

--- examples/tutorial/segments.cpp
+++ examples/tutorial/segments.cpp
@@ -14,6 +14,7 @@ int main()
   typedef viennagrid::triangular_3d_mesh                        MeshType;
 
   typedef viennagrid::result_of::vertex_handle< MeshType >::type     VertexHandleType;
+  typedef viennagrid::result_of::vertex< MeshType >::type            VertexType;
   typedef viennagrid::result_of::triangle_handle< MeshType >::type   TriangleHandleType;
   typedef viennagrid::result_of::triangle< MeshType >::type          TriangleType;
 
@@ -41,6 +42,7 @@ int main()
   TriangleHandleType th0 = viennagrid::make_triangle( mesh, vh0, vh1, vh2 );
   TriangleHandleType th1 = viennagrid::make_triangle( mesh, vh1, vh2, vh3 );
   TriangleHandleType th2 = viennagrid::make_triangle( mesh, vh3, vh4, vh5 );
+  /*TriangleHandleType th3 =*/ viennagrid::make_triangle( mesh, vh0, vh4, vh5 );
 
 
 
@@ -62,7 +64,6 @@ int main()
   std::cout << "Segments in Segmentation" << std::endl;
   for ( SegmentationType::iterator it = segmentation.begin(); it != segmentation.end(); ++it)
     std::cout << (*it).id() << std::endl;
-
   std::cout << std::endl;
 
 
@@ -70,13 +71,32 @@ int main()
   TriangleType & tri1 = viennagrid::dereference_handle(mesh, th1);
   TriangleType & tri2 = viennagrid::dereference_handle(mesh, th2);
 
-  // add triangle tr0 to seg0
-  viennagrid::add( seg0, tri0 );
-  std::cout << "Triangle 0 in Segment 0: " << viennagrid::is_in_segment( seg0, tri0 ) << std::endl;
+  VertexType & v0 = viennagrid::dereference_handle(mesh, vh0);
 
+  // add triangle tr0 to seg0
+  std::cout << "Adding element tri0 to seg0" << std::endl;
+  viennagrid::add( seg0, tri0 );
+
+  std::cout << "Adding element tri0 to seg1" << std::endl;
+  viennagrid::add( seg1, tri0 );
+
+  std::cout << "Triangle 0 in Segment 0: " << viennagrid::is_in_segment( seg0, tri0 ) << std::endl;
+  std::cout << "Vertex 0 in Segment 0: " << viennagrid::is_in_segment( seg0, v0 ) << std::endl;
+  std::cout << "Numer of cells in segmentation: " << viennagrid::cells( segmentation ).size() << std::endl;
+
+  std::cout << "Erasing element tri0 from seg0" << std::endl;
   // erase triangle tr0 from seg0
   viennagrid::erase( seg0, tri0 );
   std::cout << "Triangle 0 in Segment 0: " <<  viennagrid::is_in_segment( seg0, tri0 ) << std::endl;
+  std::cout << "Vertex 0 in Segment 0: " << viennagrid::is_in_segment( seg0, v0 ) << std::endl;
+  std::cout << "Numer of cells in segmentation: " << viennagrid::cells( segmentation ).size() << std::endl;
+
+  std::cout << "Erasing element tri0 from seg1" << std::endl;
+  // erase triangle tr0 from seg0
+  viennagrid::erase( seg1, tri0 );
+  std::cout << "Triangle 0 in Segment 0: " <<  viennagrid::is_in_segment( seg0, tri0 ) << std::endl;
+  std::cout << "Vertex 0 in Segment 0: " << viennagrid::is_in_segment( seg0, v0 ) << std::endl;
+  std::cout << "Numer of cells in segmentation: " << viennagrid::cells( segmentation ).size() << std::endl;
 
 
   // add triangle tr0 to seg0, tri1 to seg1, tri2 to seg2
@@ -88,6 +108,33 @@ int main()
   viennagrid::add( seg3, tri0 );
   viennagrid::add( seg4, tri0 );
   viennagrid::add( seg5, tri0 );
+
+
+  // print all cells of the mesh (should be four)
+  typedef viennagrid::result_of::cell_range<MeshType>::type MeshCellRange;
+  typedef viennagrid::result_of::iterator<MeshCellRange>::type MeshCellIterator;
+
+  std::cout << std::endl;
+  std::cout << "All cells in the mesh:" << std::endl;
+  MeshCellRange mesh_cells = viennagrid::elements( mesh );
+  for (MeshCellIterator cit = mesh_cells.begin(); cit != mesh_cells.end(); ++cit)
+    std::cout << *cit << std::endl;
+  std::cout << std::endl;
+
+  // print all cells of the segmentation (should be three)
+  typedef viennagrid::result_of::cell_range<SegmentationType>::type SegmentationCellRange;
+  typedef viennagrid::result_of::iterator<SegmentationCellRange>::type SegmentationCellIterator;
+
+  std::cout << std::endl;
+  std::cout << "All cells in the segmentation:" << std::endl;
+  SegmentationCellRange segmentation_cells = viennagrid::elements( segmentation );
+  for (SegmentationCellIterator cit = segmentation_cells.begin(); cit != segmentation_cells.end(); ++cit)
+    std::cout << *cit << std::endl;
+  std::cout << std::endl;
+
+
+
+
 
   typedef viennagrid::result_of::segment_id_range<SegmentationType, TriangleType>::type SegmentIDRange;
   SegmentIDRange segment_ids = viennagrid::segment_ids( segmentation, tri0 );
