62a3ea65b1ceb8b59dfe02ef8af39833b82cf910
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-01-30 19:36:39 +0100

    CTest: Updated CTestConfig.cmake to new CDash on jwein2.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,7 +1,7 @@
 set(CTEST_PROJECT_NAME "ViennaGrid")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=ViennaGrid")
+set(CTEST_DROP_SITE "jwein2.iue.tuwien.ac.at:50000")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=ViennaGrid")
 set(CTEST_DROP_SITE_CDASH TRUE)
