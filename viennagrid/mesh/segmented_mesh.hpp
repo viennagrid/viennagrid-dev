cb44c9c69fe5c4408ce8a3c84fb42315580f0a7b
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-03-05 15:42:42 +0100

    faster operator= (but only cell copying)

--- viennagrid/mesh/segmented_mesh.hpp
+++ viennagrid/mesh/segmented_mesh.hpp
@@ -39,8 +39,28 @@ namespace viennagrid
     {
       clear();
 
-      mesh = other.mesh;
-      copy_segmentation( other.mesh, other.segmentation, mesh, segmentation );
+      viennagrid::vertex_copy_map<mesh_type, mesh_type> vertex_map(mesh);
+
+      typedef typename viennagrid::result_of::cell<mesh_type>::type CellType;
+      typedef typename viennagrid::result_of::coord<mesh_type>::type NumericType;
+
+      typedef typename viennagrid::result_of::const_cell_range<mesh_type>::type ConstCellRangeType;
+      typedef typename viennagrid::result_of::iterator<ConstCellRangeType>::type ConstCellIteratorType;
+
+      typedef typename viennagrid::result_of::segment_id_range<segmentation_type, CellType>::type SrcSegmentIDRangeType;
+      typedef typename viennagrid::result_of::segment_id<segmentation_type>::type DstSegmentIDType;
+
+      typedef typename viennagrid::result_of::cell_handle<mesh_type>::type CellHandleType;
+
+      ConstCellRangeType cells(other.mesh);
+      for (ConstCellIteratorType cit = cells.begin(); cit != cells.end(); ++cit)
+      {
+        CellHandleType cell_handle = vertex_map.copy_element(*cit );
+        viennagrid::add( segmentation, cell_handle, viennagrid::segment_ids( other.segmentation, *cit ).begin(), viennagrid::segment_ids( other.segmentation, *cit ).end() );
+      }
+
+//       mesh = other.mesh;
+//       copy_segmentation( other.mesh, other.segmentation, mesh, segmentation );
 
       return *this;
     }
