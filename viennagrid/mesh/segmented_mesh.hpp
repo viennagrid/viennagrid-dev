35ddc0db8279c0913736ca2e06590566636cc064
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-03-10 18:04:05 +0100

    using cell copy function for operator=

--- viennagrid/mesh/segmented_mesh.hpp
+++ viennagrid/mesh/segmented_mesh.hpp
@@ -19,7 +19,6 @@ namespace viennagrid
   };
 
 
-
   template<typename WrappedMeshConfig, typename WrappedSegmentationConfig>
   struct segmented_mesh< viennagrid::mesh<WrappedMeshConfig>, viennagrid::segmentation<WrappedSegmentationConfig> >
   {
@@ -38,31 +37,7 @@ namespace viennagrid
 
     SelfType & operator=(SelfType const & other)
     {
-      clear();
-
-      viennagrid::vertex_copy_map<mesh_type, mesh_type> vertex_map(mesh);
-
-      typedef typename viennagrid::result_of::cell<mesh_type>::type CellType;
-      typedef typename viennagrid::result_of::coord<mesh_type>::type NumericType;
-
-      typedef typename viennagrid::result_of::const_cell_range<mesh_type>::type ConstCellRangeType;
-      typedef typename viennagrid::result_of::iterator<ConstCellRangeType>::type ConstCellIteratorType;
-
-      typedef typename viennagrid::result_of::segment_id_range<segmentation_type, CellType>::type SrcSegmentIDRangeType;
-      typedef typename viennagrid::result_of::segment_id<segmentation_type>::type DstSegmentIDType;
-
-      typedef typename viennagrid::result_of::cell_handle<mesh_type>::type CellHandleType;
-
-      ConstCellRangeType cells(other.mesh);
-      for (ConstCellIteratorType cit = cells.begin(); cit != cells.end(); ++cit)
-      {
-        CellHandleType cell_handle = vertex_map.copy_element(*cit );
-        viennagrid::add( segmentation, cell_handle, viennagrid::segment_ids( other.segmentation, *cit ).begin(), viennagrid::segment_ids( other.segmentation, *cit ).end() );
-      }
-
-//       mesh = other.mesh;
-//       copy_segmentation( other.mesh, other.segmentation, mesh, segmentation );
-
+      copy( other.mesh, other.segmentation, mesh, segmentation, true_functor() );
       return *this;
     }
 
