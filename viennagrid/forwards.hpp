226629deed88b94e2e0a6b9db9bb2edac224c234
Karl Rupp <rupp@iue.tuwien.ac.at>
2014-04-15 03:30:00 +0200

    Updated version number to 2.1.0.

--- viennagrid/forwards.hpp
+++ viennagrid/forwards.hpp
@@ -14,7 +14,7 @@
 ======================================================================= */
 
 /**
- @mainpage Source Code Documentation for ViennaGrid 2.0.0
+ @mainpage Source Code Documentation for ViennaGrid 2.1.0
 
  This is the source code documentation of ViennaGrid. Detailed information about the functions in ViennaGrid can be found here.
 
@@ -29,7 +29,7 @@
 #include <cstdlib>     //for EXIT_SUCCESS and EXIT_FAILURE
 
 #define VIENNAGRID_MAJOR_VERSION   2
-#define VIENNAGRID_MINOR_VERSION   0
+#define VIENNAGRID_MINOR_VERSION   1
 #define VIENNAGRID_BUGFIX_LEVEL    0
 
 
