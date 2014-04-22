f15cafa1a7326ff0d1e9772329a3b1a419d44d15
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-04-22 12:56:46 +0200

    moved hyperplane implementation to refine.hpp

--- viennagrid/algorithm/refine.hpp
+++ viennagrid/algorithm/refine.hpp
@@ -231,144 +231,183 @@ namespace viennagrid
 
 
 
-    /** @brief Ensures refinement of the longest edge of each cell. If any edge is tagged for refinement in a cell, then the longest edge is refined as well. */
-    template<typename CellTagIn, typename WrappedMeshConfigInT, typename EdgeRefinementFlagAccessorT>
-    void ensure_longest_edge_refinement(mesh<WrappedMeshConfigInT> const & mesh_in, EdgeRefinementFlagAccessorT edge_refinement_flag_accessor)
+  /** @brief Ensures refinement of the longest edge of each cell. If any edge is tagged for refinement in a cell, then the longest edge is refined as well. */
+  template<typename CellTagIn, typename WrappedMeshConfigInT, typename EdgeRefinementFlagAccessorT>
+  void ensure_longest_edge_refinement(mesh<WrappedMeshConfigInT> const & mesh_in, EdgeRefinementFlagAccessorT edge_refinement_flag_accessor)
+  {
+    typedef mesh<WrappedMeshConfigInT>                                            MeshInType;
+
+    typedef typename viennagrid::result_of::element<MeshInType, vertex_tag>::type     VertexType;
+    typedef typename viennagrid::result_of::element<MeshInType, line_tag>::type       EdgeType;
+    typedef typename viennagrid::result_of::element<MeshInType, CellTagIn>::type      CellType;
+
+    typedef typename viennagrid::result_of::const_element_range<MeshInType, CellTagIn>::type    CellRange;
+    typedef typename viennagrid::result_of::iterator<CellRange>::type                             CellIterator;
+    typedef typename viennagrid::result_of::const_element_range<CellType, line_tag>::type         EdgeOnCellRange;
+    typedef typename viennagrid::result_of::iterator<EdgeOnCellRange>::type                       EdgeOnCellIterator;
+    typedef typename viennagrid::result_of::const_element_range<EdgeType, vertex_tag>::type       VertexOnEdgeRange;
+    typedef typename viennagrid::result_of::iterator<VertexOnEdgeRange>::type                     VertexOnEdgeIterator;
+
+
+    bool something_changed = true;
+    //std::size_t iter_cnt = 0;
+
+    while (something_changed)
     {
-      typedef mesh<WrappedMeshConfigInT>                                            MeshInType;
+      something_changed = false;
 
-      typedef typename viennagrid::result_of::element<MeshInType, vertex_tag>::type     VertexType;
-      typedef typename viennagrid::result_of::element<MeshInType, line_tag>::type       EdgeType;
-      typedef typename viennagrid::result_of::element<MeshInType, CellTagIn>::type      CellType;
-
-      typedef typename viennagrid::result_of::const_element_range<MeshInType, CellTagIn>::type    CellRange;
-      typedef typename viennagrid::result_of::iterator<CellRange>::type                             CellIterator;
-      typedef typename viennagrid::result_of::const_element_range<CellType, line_tag>::type         EdgeOnCellRange;
-      typedef typename viennagrid::result_of::iterator<EdgeOnCellRange>::type                       EdgeOnCellIterator;
-      typedef typename viennagrid::result_of::const_element_range<EdgeType, vertex_tag>::type       VertexOnEdgeRange;
-      typedef typename viennagrid::result_of::iterator<VertexOnEdgeRange>::type                     VertexOnEdgeIterator;
-
-
-      bool something_changed = true;
-      //std::size_t iter_cnt = 0;
-
-      while (something_changed)
-      {
-        something_changed = false;
-
-        CellRange cells = viennagrid::elements<CellTagIn>(mesh_in);
-        for (CellIterator cit  = cells.begin();
-                          cit != cells.end();
-                        ++cit)
-        {
-          //
-          // check if any edges are tagged for refinement
-          //
-          bool has_refinement = false;
-          EdgeOnCellRange edges_on_cell = viennagrid::elements<line_tag>(*cit);
-          for (EdgeOnCellIterator eocit = edges_on_cell.begin();
-                                  eocit != edges_on_cell.end();
-                                ++eocit)
-          {
-            if ( edge_refinement_flag_accessor(*eocit) )
-            {
-              has_refinement = true;
-              break;
-            }
-          }
-
-          if (has_refinement)
-          {
-            //
-            // Find longest edge
-            //
-            EdgeType const * longest_edge_ptr = NULL;
-            double longest_edge_len = 0;
-
-            for (EdgeOnCellIterator eocit = edges_on_cell.begin();
-                                    eocit != edges_on_cell.end();
-                                  ++eocit)
-            {
-              VertexOnEdgeRange vertices_on_edge = viennagrid::elements<vertex_tag>(*eocit);
-              VertexOnEdgeIterator voeit = vertices_on_edge.begin();
-              VertexType const & v0 = *voeit; ++voeit;
-              VertexType const & v1 = *voeit;
-
-              double len = viennagrid::norm( viennagrid::point(mesh_in, v0) - viennagrid::point(mesh_in, v1) );
-              if (len > longest_edge_len)
-              {
-                longest_edge_len = len;
-                longest_edge_ptr = &(*eocit);
-              }
-            }
-
-            //tag longest edge:
-            if ( !edge_refinement_flag_accessor(*longest_edge_ptr) )
-            {
-              edge_refinement_flag_accessor(*longest_edge_ptr) = true;
-              something_changed = true;
-            }
-          }
-
-        } //for cells
-
-
-      } //while
-    } //ensure_longest_edge_refinement
-
-    /** @brief Transfers tags for refinement from the cell to edges */
-    template<typename CellTagIn, typename WrappedMeshConfigInT, typename CellRefinementFlagAccessorT, typename EdgeRefinementFlagAccessorT>
-    void cell_refinement_to_edge_refinement(mesh<WrappedMeshConfigInT> const & mesh_in,
-                                            CellRefinementFlagAccessorT cell_refinement_flag, EdgeRefinementFlagAccessorT edge_refinement_flag_accessor)
-    {
-      typedef mesh<WrappedMeshConfigInT>                                                        MeshInType;
-
-      typedef typename viennagrid::result_of::element<MeshInType, CellTagIn>::type                  CellType;
-
-      typedef typename viennagrid::result_of::const_element_range<MeshInType, CellTagIn>::type      CellRange;
-      typedef typename viennagrid::result_of::iterator<CellRange>::type                               CellIterator;
-      typedef typename viennagrid::result_of::const_element_range<CellType, line_tag>::type           EdgeOnCellRange;
-      typedef typename viennagrid::result_of::iterator<EdgeOnCellRange>::type                         EdgeOnCellIterator;
-
-      std::size_t cells_for_refinement = 0;
-      CellRange cells(mesh_in);
+      CellRange cells = viennagrid::elements<CellTagIn>(mesh_in);
       for (CellIterator cit  = cells.begin();
                         cit != cells.end();
                       ++cit)
       {
-        if ( cell_refinement_flag(*cit) )
+        //
+        // check if any edges are tagged for refinement
+        //
+        bool has_refinement = false;
+        EdgeOnCellRange edges_on_cell = viennagrid::elements<line_tag>(*cit);
+        for (EdgeOnCellIterator eocit = edges_on_cell.begin();
+                                eocit != edges_on_cell.end();
+                              ++eocit)
         {
-          ++cells_for_refinement;
+          if ( edge_refinement_flag_accessor(*eocit) )
+          {
+            has_refinement = true;
+            break;
+          }
+        }
 
-          EdgeOnCellRange edges_on_cell(*cit);
+        if (has_refinement)
+        {
+          //
+          // Find longest edge
+          //
+          EdgeType const * longest_edge_ptr = NULL;
+          double longest_edge_len = 0;
+
           for (EdgeOnCellIterator eocit = edges_on_cell.begin();
                                   eocit != edges_on_cell.end();
                                 ++eocit)
           {
-            edge_refinement_flag_accessor(*eocit) = true;
+            VertexOnEdgeRange vertices_on_edge = viennagrid::elements<vertex_tag>(*eocit);
+            VertexOnEdgeIterator voeit = vertices_on_edge.begin();
+            VertexType const & v0 = *voeit; ++voeit;
+            VertexType const & v1 = *voeit;
+
+            double len = viennagrid::norm( viennagrid::point(mesh_in, v0) - viennagrid::point(mesh_in, v1) );
+            if (len > longest_edge_len)
+            {
+              longest_edge_len = len;
+              longest_edge_ptr = &(*eocit);
+            }
+          }
+
+          //tag longest edge:
+          if ( !edge_refinement_flag_accessor(*longest_edge_ptr) )
+          {
+            edge_refinement_flag_accessor(*longest_edge_ptr) = true;
+            something_changed = true;
           }
         }
-      }
 
-    }
+      } //for cells
 
 
-    /** @brief Mark all edges for refinement -> unform refinement */
-    template<typename WrappedMeshConfigInT, typename EdgeRefinementFlagAccessorT>
-    void mark_all_edge_refinement(mesh<WrappedMeshConfigInT> const & mesh_in, EdgeRefinementFlagAccessorT edge_refinement_flag_accessor)
+    } //while
+  } //ensure_longest_edge_refinement
+
+  /** @brief Transfers tags for refinement from the cell to edges */
+  template<typename CellTagIn, typename WrappedMeshConfigInT, typename CellRefinementFlagAccessorT, typename EdgeRefinementFlagAccessorT>
+  void cell_refinement_to_edge_refinement(mesh<WrappedMeshConfigInT> const & mesh_in,
+                                          CellRefinementFlagAccessorT cell_refinement_flag, EdgeRefinementFlagAccessorT edge_refinement_flag_accessor)
+  {
+    typedef mesh<WrappedMeshConfigInT>                                                        MeshInType;
+
+    typedef typename viennagrid::result_of::element<MeshInType, CellTagIn>::type                  CellType;
+
+    typedef typename viennagrid::result_of::const_element_range<MeshInType, CellTagIn>::type      CellRange;
+    typedef typename viennagrid::result_of::iterator<CellRange>::type                               CellIterator;
+    typedef typename viennagrid::result_of::const_element_range<CellType, line_tag>::type           EdgeOnCellRange;
+    typedef typename viennagrid::result_of::iterator<EdgeOnCellRange>::type                         EdgeOnCellIterator;
+
+    std::size_t cells_for_refinement = 0;
+    CellRange cells(mesh_in);
+    for (CellIterator cit  = cells.begin();
+                      cit != cells.end();
+                    ++cit)
     {
-      typedef mesh<WrappedMeshConfigInT>                                                                MeshInType;
+      if ( cell_refinement_flag(*cit) )
+      {
+        ++cells_for_refinement;
 
-      typedef typename viennagrid::result_of::const_element_range<MeshInType, viennagrid::line_tag>::type   LineRange;
-      typedef typename viennagrid::result_of::iterator<LineRange>::type                                       LineIterator;
-
-      LineRange lines(mesh_in);
-      for (LineIterator it = lines.begin(); it != lines.end(); ++it)
-        edge_refinement_flag_accessor(*it) = true;
+        EdgeOnCellRange edges_on_cell(*cit);
+        for (EdgeOnCellIterator eocit = edges_on_cell.begin();
+                                eocit != edges_on_cell.end();
+                              ++eocit)
+        {
+          edge_refinement_flag_accessor(*eocit) = true;
+        }
+      }
     }
 
+  }
 
 
+  /** @brief Mark all edges for refinement -> unform refinement */
+  template<typename WrappedMeshConfigInT, typename EdgeRefinementFlagAccessorT>
+  void mark_all_edge_refinement(mesh<WrappedMeshConfigInT> const & mesh_in, EdgeRefinementFlagAccessorT edge_refinement_flag_accessor)
+  {
+    typedef mesh<WrappedMeshConfigInT>                                                                MeshInType;
+
+    typedef typename viennagrid::result_of::const_element_range<MeshInType, viennagrid::line_tag>::type   LineRange;
+    typedef typename viennagrid::result_of::iterator<LineRange>::type                                       LineIterator;
+
+    LineRange lines(mesh_in);
+    for (LineIterator it = lines.begin(); it != lines.end(); ++it)
+      edge_refinement_flag_accessor(*it) = true;
+  }
+
+
+  /** @brief Mark edges for refinement based on a hyperplane */
+  template<typename SrcMeshT, typename DstMeshT, typename PointT, typename LineRefinementTagContainerT, typename LineRefinementVertexHandleContainerT, typename NumericConfigT>
+  void mark_edges_for_hyperplane_refine(SrcMeshT const & src_mesh, DstMeshT & dst_mesh,
+                            PointT const & hyperplane_point, PointT const & hyperplane_normal,
+                            LineRefinementTagContainerT & line_refinement_tag_accessor,
+                            LineRefinementVertexHandleContainerT & line_refinement_vertex_handle_accessor,
+                            NumericConfigT numeric_config)
+  {
+    typedef typename viennagrid::result_of::coord<SrcMeshT>::type NumericType;
+    typedef typename viennagrid::result_of::const_line_range<SrcMeshT>::type ConstLineRangeType;
+    typedef typename viennagrid::result_of::iterator<ConstLineRangeType>::type ConstLineIteratorType;
+
+    ConstLineRangeType lines(src_mesh);
+    for (ConstLineIteratorType lit = lines.begin(); lit != lines.end(); ++lit)
+    {
+      NumericType distance0 = viennagrid::inner_prod( hyperplane_normal, viennagrid::point( viennagrid::vertices(*lit)[0] )-hyperplane_point );
+      NumericType distance1 = viennagrid::inner_prod( hyperplane_normal, viennagrid::point( viennagrid::vertices(*lit)[1] )-hyperplane_point );
+
+      NumericType tolerance = viennagrid::detail::relative_tolerance(numeric_config, viennagrid::volume(*lit));
+
+      if (distance0 > distance1)
+        std::swap(distance0, distance1);
+
+      if (distance0 < -tolerance && distance1 > tolerance)
+      {
+        line_refinement_tag_accessor(*lit) = true;
+
+        PointT const & pt0 = viennagrid::point( viennagrid::vertices(*lit)[0] );
+        PointT const & pt1 = viennagrid::point( viennagrid::vertices(*lit)[1] );
+
+        double qd = viennagrid::inner_prod( hyperplane_normal, pt0-hyperplane_point );
+        double pd = viennagrid::inner_prod( hyperplane_normal, pt1-hyperplane_point );
+
+        PointT new_pt = pt1 - (pd / (pd-qd)) * (pt1 - pt0);
+        line_refinement_vertex_handle_accessor(*lit) = viennagrid::make_unique_vertex( dst_mesh, new_pt );
+      }
+      else
+        line_refinement_tag_accessor(*lit) = false;
+    }
+  }
 
 
 
@@ -626,6 +665,41 @@ namespace viennagrid
     typedef typename viennagrid::result_of::cell<MeshInType>::type    CellType;
 
     refine_uniformly<CellType>(mesh_in, mesh_out);
+  }
+
+  /** @brief Refines a mesh based on a hyperplane. All elements which intersects the hyperplane are refined in a way that they don't intersect the hyperplane afterwards.
+   *
+   * @param src_mesh            The input mesh
+   * @param dst_mesh            The output mesh
+   * @param hyperplane_point    A point representing the hyperplane
+   * @param hyperplane_normal   The normale vector representing the hyperplane
+   * @param numeric_config      The numeric config
+   */
+  template<typename SrcMeshT, typename DstMeshT, typename PointT, typename NumericConfigT>
+  void hyperplane_refine(SrcMeshT const & src_mesh, DstMeshT & dst_mesh,
+                         PointT const & hyperplane_point, PointT const & hyperplane_normal,
+                         NumericConfigT numeric_config)
+  {
+    typedef typename viennagrid::result_of::cell_tag<SrcMeshT>::type CellTag;
+    typedef typename viennagrid::result_of::vertex<SrcMeshT>::type VertexType;
+    typedef typename viennagrid::result_of::line<SrcMeshT>::type LineType;
+
+    typedef typename viennagrid::result_of::vertex_handle<DstMeshT>::type DstMeshVertexHandleType;
+
+    viennagrid::vertex_copy_map<SrcMeshT, DstMeshT> vertex_map( dst_mesh );
+
+    std::deque<bool> line_refinement_tag_container;
+    typename viennagrid::result_of::accessor<std::deque<bool>, LineType>::type line_refinement_tag_accessor(line_refinement_tag_container);
+
+    std::deque<DstMeshVertexHandleType> line_refinement_vertex_handle_container;
+    typename viennagrid::result_of::accessor<std::deque<DstMeshVertexHandleType>, LineType>::type line_refinement_vertex_handle_accessor(line_refinement_vertex_handle_container);
+
+    mark_edges_for_hyperplane_refine(src_mesh, dst_mesh, hyperplane_point, hyperplane_normal, line_refinement_tag_accessor, line_refinement_vertex_handle_accessor, numeric_config);
+
+    viennagrid::simple_refine<CellTag>(src_mesh, dst_mesh,
+                                       vertex_map,
+                                       line_refinement_tag_accessor,
+                                       line_refinement_vertex_handle_accessor);
   }
 
 
@@ -947,6 +1021,46 @@ namespace viennagrid
     typedef mesh<WrappedMeshConfigInT>                            MeshInType;
     typedef typename viennagrid::result_of::cell<MeshInType>::type    CellType;
     refine_uniformly<CellType>(mesh_in, segmentation_in, mesh_out, segmentation_out);
+  }
+
+  /** @brief Refines a mesh and a segmentation based on a hyperplane. All elements which intersects the hyperplane are refined in a way that they don't intersect the hyperplane afterwards.
+   *
+   * @param src_mesh            The input mesh
+   * @param src_segmentation    The input segmentation
+   * @param dst_mesh            The output mesh
+   * @param dst_segmentation    The output segmentation
+   * @param hyperplane_point    A point representing the hyperplane
+   * @param hyperplane_normal   The normale vector representing the hyperplane
+   * @param numeric_config      The numeric config
+   */
+  template<typename SrcMeshT, typename SrcSegmentationT, typename DstMeshT, typename DstSegmentationT, typename PointT, typename NumericConfigT>
+  void hyperplane_refine(SrcMeshT const & src_mesh, SrcSegmentationT const & src_segmentation,
+                         DstMeshT & dst_mesh, DstSegmentationT & dst_segmentation,
+                         PointT const & hyperplane_point, PointT const & hyperplane_normal,
+                         NumericConfigT numeric_config)
+  {
+    typedef typename viennagrid::result_of::cell_tag<SrcMeshT>::type CellTag;
+    typedef typename viennagrid::result_of::vertex<SrcMeshT>::type VertexType;
+    typedef typename viennagrid::result_of::line<SrcMeshT>::type LineType;
+    typedef typename viennagrid::result_of::vertex_handle<DstMeshT>::type DstMeshVertexHandleType;
+    typedef typename viennagrid::result_of::point<SrcMeshT>::type PointType;
+
+    viennagrid::vertex_copy_map<SrcMeshT, DstMeshT> vertex_map( dst_mesh );
+
+    std::deque<bool> line_refinement_tag_container;
+    typename viennagrid::result_of::accessor<std::deque<bool>, LineType>::type line_refinement_tag_accessor(line_refinement_tag_container);
+
+    std::deque<DstMeshVertexHandleType> line_refinement_vertex_handle_container;
+    typename viennagrid::result_of::accessor<std::deque<DstMeshVertexHandleType>, LineType>::type line_refinement_vertex_handle_accessor(line_refinement_vertex_handle_container);
+
+    mark_edges_for_hyperplane_refine(src_mesh, dst_mesh, hyperplane_point, hyperplane_normal, line_refinement_tag_accessor, line_refinement_vertex_handle_accessor, numeric_config);
+
+
+    viennagrid::simple_refine<CellTag>(src_mesh, src_segmentation,
+                                       dst_mesh, dst_segmentation,
+                                       vertex_map,
+                                       line_refinement_tag_accessor,
+                                       line_refinement_vertex_handle_accessor);
   }
 
 
