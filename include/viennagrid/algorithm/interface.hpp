99b475e386c1a66f7a4d82d8997f269f01e5ba14
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2014-11-17 15:49:10 +0100

    commit before remove

--- include/viennagrid/algorithm/interface.hpp
+++ include/viennagrid/algorithm/interface.hpp
@@ -1,16 +1,4 @@
 #ifndef VIENNAGRID_ALGORITHM_INTERFACE_HPP
 #define VIENNAGRID_ALGORITHM_INTERFACE_HPP
 
-#include "viennagrid/core.hpp"
-
-namespace viennagrid
-{
-  template<typename SomethingFirstT, typename SomethingSecondT, typename ElementT>
-  bool is_interface( SomethingFirstT first, SomethingSecondT second, ElementT element )
-  {
-    return is_boundary(first, element) && is_boundary(second, element);
-  }
-}
-
-
 #endif
