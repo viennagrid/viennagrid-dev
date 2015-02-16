0279fd7c12e7e0782b90ca48daacc8455f512711
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-02-16 17:25:40 +0100

    cleanup

--- tests/src/named_segment.cpp
+++ tests/src/named_segment.cpp
@@ -37,8 +37,7 @@ inline std::string random_name()
 }
 
 
-template<int dimension, typename CellTagT>
-void test(int segment_count, int test_count)
+void test(int region_count, int test_count)
 {
   typedef viennagrid::mesh_t MeshType;
   for (int i = 0; i < test_count; ++i)
@@ -46,7 +45,7 @@ void test(int segment_count, int test_count)
     MeshType mesh;
 
     std::map<std::string, int> segment_names;
-    while (static_cast<int>(segment_names.size()) != segment_count)
+    while (static_cast<int>(segment_names.size()) != region_count)
       segment_names[random_name()] = rand() % 90 + 10;
 
     for (std::map<std::string, int>::const_iterator sit = segment_names.begin(); sit != segment_names.end(); ++sit)
@@ -60,7 +59,9 @@ void test(int segment_count, int test_count)
     {
       if (static_cast<int>(viennagrid::vertices( mesh.get_region(sit->first) ).size()) != sit->second)
       {
-        std::cerr << "Numbers of vertices in a named segment is equal to created vertices" << std::endl;
+        std::cerr << "Number of vertices in a named region is NOT equal to created vertices" << std::endl;
+        std::cerr << "Vertex count created: " << sit->second << std::endl;
+        std::cerr << "Vertex count in region: " << viennagrid::vertices( mesh.get_region(sit->first) ).size() << std::endl;
         std::cerr << "FAILED!" << std::endl;
         exit(EXIT_FAILURE);
       }
@@ -72,22 +73,15 @@ int main()
 {
   srand (static_cast<unsigned int>(time(NULL)));
 
-  int segment_count = 10;
+  int region_count = 10;
   int test_count = 10;
 
   std::cout << "*****************" << std::endl;
   std::cout << "* Test started! *" << std::endl;
   std::cout << "*****************" << std::endl;
 
-  std::cout << "--- Testing with triangular_2d ---" << std::endl;
-  test<2, viennagrid::triangle_tag>(segment_count, test_count);
-  std::cout << "--- Testing with tetrahedral_3d ---" << std::endl;
-  test<3, viennagrid::tetrahedron_tag>(segment_count, test_count);
-
-  std::cout << "--- Testing with quadrilateral_2d ---" << std::endl;
-  test<2, viennagrid::quadrilateral_tag>(segment_count, test_count);
-  std::cout << "--- Testing with hexahedral_3d ---" << std::endl;
-  test<3, viennagrid::hexahedron_tag>(segment_count, test_count);
+  std::cout << "--- Testing---" << std::endl;
+  test(region_count, test_count);
 
   std::cout << "*******************************" << std::endl;
   std::cout << "* Test finished successfully! *" << std::endl;
