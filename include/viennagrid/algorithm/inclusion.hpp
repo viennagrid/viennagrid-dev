e40e4175c646b1902ecbf982c7fef5c379c99519
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-02-26 18:34:12 +0100

    coordinate version of inclusion, added inclusion test to quadrilateral

--- include/viennagrid/algorithm/inclusion.hpp
+++ include/viennagrid/algorithm/inclusion.hpp
@@ -29,21 +29,16 @@ namespace viennagrid
   {
     // http://www.blackpawn.com/texts/pointinpoly/
     /** @brief Is inside implementation for triangle in arbitrary dimensions. */
-    template<typename PointAccessorT, typename ElementT, typename PointT, typename NumericConfigT>
-    bool is_inside_impl( PointAccessorT const accessor,
-                         ElementT const & element, viennagrid::triangle_tag,
-                         PointT const & p,
-                         NumericConfigT numeric_config )
+    template<typename PointT, typename NumericConfigT>
+    bool is_inside_triangle(PointT const & tp0, PointT const & tp1, PointT const & tp2,
+                            PointT const & p,
+                            NumericConfigT numeric_config)
     {
       typedef typename viennagrid::result_of::coord<PointT>::type NumericType;
 
-      PointT a = accessor.get( viennagrid::vertices(element)[0] );
-      PointT b = accessor.get( viennagrid::vertices(element)[1] );
-      PointT c = accessor.get( viennagrid::vertices(element)[2] );
-
-      PointT v0 = c-a;
-      PointT v1 = b-a;
-      PointT v2 = p-a;
+      PointT v0 = tp2-tp0;
+      PointT v1 = tp1-tp0;
+      PointT v2 = p-tp0;
 
       NumericType dot00 = viennagrid::inner_prod(v0, v0);
       NumericType dot01 = viennagrid::inner_prod(v0, v1);
@@ -59,31 +54,69 @@ namespace viennagrid
       return (u >= -abs_eps) && (v >= -abs_eps) && (u+v <= static_cast<NumericType>(1) + NumericType(2.0)*abs_eps );
     }
 
+    /** @brief Is inside implementation for tetrahedron. */
+    template<typename PointAccessorT, typename ElementT, typename PointT, typename NumericConfigT>
+    bool is_inside_impl(PointAccessorT const accessor,
+                        ElementT const & element, viennagrid::triangle_tag,
+                        PointT const & p,
+                        NumericConfigT numeric_config )
+    {
+      return is_inside_triangle( accessor.get(viennagrid::vertices(element)[0]),
+                                 accessor.get(viennagrid::vertices(element)[1]),
+                                 accessor.get(viennagrid::vertices(element)[2]),
+                                 p, numeric_config );
+    }
 
     /** @brief Is inside implementation for tetrahedron. */
     template<typename PointAccessorT, typename ElementT, typename PointT, typename NumericConfigT>
-    bool is_inside_impl( PointAccessorT const accessor,
-                         ElementT const & element, viennagrid::tetrahedron_tag,
-                         PointT const & p,
-                         NumericConfigT numeric_config )
+    bool is_inside_impl(PointAccessorT const accessor,
+                        ElementT const & element, viennagrid::quadrilateral_tag,
+                        PointT const & p,
+                        NumericConfigT numeric_config )
+    {
+      return is_inside_triangle( accessor.get(viennagrid::vertices(element)[0]),
+                                 accessor.get(viennagrid::vertices(element)[1]),
+                                 accessor.get(viennagrid::vertices(element)[2]),
+                                 p, numeric_config ) ||
+             is_inside_triangle( accessor.get(viennagrid::vertices(element)[1]),
+                                 accessor.get(viennagrid::vertices(element)[2]),
+                                 accessor.get(viennagrid::vertices(element)[3]),
+                                 p, numeric_config );
+    }
+
+
+
+    /** @brief Is inside implementation for tetrahedron. */
+    template<typename PointT, typename NumericConfigT>
+    bool is_inside_tetrahedron(PointT const & tp0, PointT const & tp1, PointT const & tp2, PointT const & tp3,
+                               PointT const & p,
+                               NumericConfigT numeric_config )
     {
       typedef typename viennagrid::result_of::coord<PointT>::type NumericType;
 
-      PointT a = accessor.get( viennagrid::vertices(element)[0] );
-      PointT b = accessor.get( viennagrid::vertices(element)[1] );
-      PointT c = accessor.get( viennagrid::vertices(element)[2] );
-      PointT d = accessor.get( viennagrid::vertices(element)[3] );
+      NumericType denom = static_cast<NumericType>(1) / spanned_volume(tp0, tp1, tp2, tp3);
 
-
-      NumericType denom = static_cast<NumericType>(1) / spanned_volume(a,b,c,d);
-
-      NumericType A = spanned_volume(p,b,c,d) * denom;
-      NumericType B = spanned_volume(a,p,c,d) * denom;
-      NumericType C = spanned_volume(a,b,p,d) * denom;
-      NumericType D = spanned_volume(a,b,c,p) * denom;
+      NumericType A = spanned_volume(  p,tp1,tp2,tp3) * denom;
+      NumericType B = spanned_volume(tp0,  p,tp2,tp3) * denom;
+      NumericType C = spanned_volume(tp0,tp1,  p,tp3) * denom;
+      NumericType D = spanned_volume(tp0,tp1,tp2,  p) * denom;
 
       NumericType abs_eps = absolute_tolerance<NumericType>(numeric_config);
       return (A >= -abs_eps) && (B >= -abs_eps) && (C >= -abs_eps) && (D >= -abs_eps) && (A+B+C+D <= static_cast<NumericType>(1) + NumericType(2.0)*abs_eps);
+    }
+
+    /** @brief Is inside implementation for tetrahedron. */
+    template<typename PointAccessorT, typename ElementT, typename PointT, typename NumericConfigT>
+    bool is_inside_impl(PointAccessorT const accessor,
+                        ElementT const & element, viennagrid::tetrahedron_tag,
+                        PointT const & p,
+                        NumericConfigT numeric_config )
+    {
+      return is_inside_tetrahedron( accessor.get(viennagrid::vertices(element)[0]),
+                                    accessor.get(viennagrid::vertices(element)[1]),
+                                    accessor.get(viennagrid::vertices(element)[2]),
+                                    accessor.get(viennagrid::vertices(element)[3]),
+                                    p, numeric_config );
     }
 
 
@@ -96,6 +129,8 @@ namespace viennagrid
     {
       if (element.tag() == viennagrid::triangle_tag())
         return is_inside_impl(accessor, element, triangle_tag(), p, numeric_config);
+      if (element.tag() == viennagrid::quadrilateral_tag())
+        return is_inside_impl(accessor, element, quadrilateral_tag(), p, numeric_config);
       if (element.tag() == viennagrid::tetrahedron_tag())
         return is_inside_impl(accessor, element, tetrahedron_tag(), p, numeric_config);
 
