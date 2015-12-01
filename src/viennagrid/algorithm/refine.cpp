b1441eb133e37b916e61c0585efff4081aefe078
Florian Rudolf <rudolf@iue.tuwien.ac.at>
2015-12-01 16:47:16 +0100

    edge refinement for triangles: choose shortest diagonal  when refining two edges

--- src/viennagrid/algorithm/refine.cpp
+++ src/viennagrid/algorithm/refine.cpp
@@ -14,6 +14,12 @@ static viennagrid_error edge_refine_triangle(viennagrid_mesh         mesh,
   viennagrid_int num_edges_to_refine = 0;
   viennagrid_element_id new_vertices[3];
 
+  viennagrid_dimension dim;
+  RETURN_ON_ERROR( viennagrid_mesh_geometric_dimension_get(mesh, &dim) );
+
+  viennagrid_numeric * coords;
+  RETURN_ON_ERROR( viennagrid_mesh_vertex_coords_pointer(mesh, &coords) );
+
   viennagrid_element_id *edges_begin, *edges_end;
   RETURN_ON_ERROR( viennagrid_element_boundary_elements(mesh, cell_id, 1, &edges_begin, &edges_end); );
   for (viennagrid_element_id *eit  = edges_begin;
@@ -97,82 +103,106 @@ static viennagrid_error edge_refine_triangle(viennagrid_mesh         mesh,
   }
   else if (num_edges_to_refine == 2) // note: for (hopefully) better maintainability and better understanding each case is coded explicitly
   {
+    viennagrid_element_id v0_id = -1;
+    viennagrid_element_id v1_id = -1;
+    viennagrid_element_id v2_id = -1;
+
+    viennagrid_element_id vX1_id = -1;
+    viennagrid_element_id vX2_id = -1;
+
+    /*
+    * assign indices that the triangle locally looks like:
+    *
+    *             2
+    *           /  \
+    *          /    \
+    *        X2      \
+    *        /        \
+    *       0 -- X1 -- 1
+    */
+
     if (edge_refinement_tags[viennagrid_index_from_element_id(edges_begin[0])] && edge_refinement_tags[viennagrid_index_from_element_id(edges_begin[1])])
     {
       /*
-      *            2
-      *          /   \
-      *         X     \
-      *        /       \
-      *       0 -- X -- 1
-      */
-      new_vertices[0] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[0])];
-      new_vertices[1] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[0])];
-      new_vertices[2] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[1])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+       * vertex index 0 -> 0, 1 -> 1, 2 -> 2
+       * vX1 = edge 0
+       * vX2 = edge 1
+       */
 
-      // TODO: Use stability criterion, e.g. split longer edge? This should be optional.
-      new_vertices[0] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[0])];
-      new_vertices[1] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[1])];
-      new_vertices[2] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[2])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+      v0_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[0])];
+      v1_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[1])];
+      v2_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[2])];
 
-      new_vertices[0] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[0])];
-      new_vertices[1] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[2])];
-      new_vertices[2] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[1])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+      vX1_id = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[0])];
+      vX2_id = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[1])];
     }
     else if (edge_refinement_tags[viennagrid_index_from_element_id(edges_begin[0])] && edge_refinement_tags[viennagrid_index_from_element_id(edges_begin[2])])
     {
       /*
-      *            2
-      *          /   \
-      *         /     X
-      *        /       \
-      *       0 -- X -- 1
-      */
-      new_vertices[0] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[0])];
-      new_vertices[1] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[1])];
-      new_vertices[2] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[2])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+       * vertex index 1 -> 0, 2 -> 1, 0 -> 2
+       * vX1 = edge 2
+       * vX2 = edge 0
+       */
 
-      // TODO: Use stability criterion, e.g. split longer edge? This should be optional.
-      new_vertices[0] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[0])];
-      new_vertices[1] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[0])];
-      new_vertices[2] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[2])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+      v0_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[1])];
+      v1_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[2])];
+      v2_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[0])];
 
-      new_vertices[0] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[0])];
-      new_vertices[1] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[2])];
-      new_vertices[2] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[2])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+      vX1_id = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[2])];
+      vX2_id = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[0])];
     }
     else if (edge_refinement_tags[viennagrid_index_from_element_id(edges_begin[1])] && edge_refinement_tags[viennagrid_index_from_element_id(edges_begin[2])])
     {
       /*
-      *            2
-      *          /   \
-      *         X     X
-      *        /       \
-      *       0 - - - - 1
-      */
-      new_vertices[0] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[2])];
-      new_vertices[1] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[2])];
-      new_vertices[2] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[1])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+       * vertex index 2 -> 0, 0 -> 1, 1 -> 2
+       * vX1 = edge 1
+       * vX2 = edge 2
+       */
 
-      // TODO: Use stability criterion, e.g. split longer edge? This should be optional.
-      new_vertices[0] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[0])];
-      new_vertices[1] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[1])];
-      new_vertices[2] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[1])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+      v0_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[2])];
+      v1_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[0])];
+      v2_id = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[1])];
 
-      new_vertices[0] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[1])];
-      new_vertices[1] = old_to_new_vertex_map[viennagrid_index_from_element_id(vertices_begin[1])];
-      new_vertices[2] = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[2])];
-      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+      vX1_id = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[1])];
+      vX2_id = vertex_on_edge_ids[viennagrid_index_from_element_id(edges_begin[2])];
     }
 
+
+    viennagrid_numeric d_X1_2;
+    viennagrid_numeric d_X2_1;
+
+    RETURN_ON_ERROR( viennagrid_distance_2(dim, coords+vX1_id*dim, coords+v2_id*dim, &d_X1_2) );
+    RETURN_ON_ERROR( viennagrid_distance_2(dim, coords+vX2_id*dim, coords+v1_id*dim, &d_X2_1) );
+
+    new_vertices[0] = v0_id;
+    new_vertices[1] = vX1_id;
+    new_vertices[2] = vX2_id;
+    RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+
+    if (d_X1_2 < d_X2_1)
+    {
+      new_vertices[0] = vX2_id;
+      new_vertices[1] = vX1_id;
+      new_vertices[2] = v2_id;
+      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+
+      new_vertices[0] = vX1_id;
+      new_vertices[1] = v1_id;
+      new_vertices[2] = v2_id;
+      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+    }
+    else
+    {
+      new_vertices[0] = vX2_id;
+      new_vertices[1] = vX1_id;
+      new_vertices[2] = v1_id;
+      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+
+      new_vertices[0] = vX2_id;
+      new_vertices[1] = v1_id;
+      new_vertices[2] = v2_id;
+      RETURN_ON_ERROR( viennagrid_mesh_element_create(output_mesh, VIENNAGRID_ELEMENT_TYPE_TRIANGLE, 3, new_vertices, NULL) );
+    }
   }
   else // refine each edge
   {
